300dc9132be5ef777f2cdb6904ee45f12d3c2100
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2025-12-09 08:29:53 -0500

    Initial work to make a mod dependent on memhack extension

---
+++ RedactedRiceMods/mods/PilotSkills/scripts/init.lua
@@ -0,0 +1,23 @@
+local mod = {
+	id = "redactedrice_pilotskillswapper",
+	name = "Pilot Skill Swap",
+	icon = "img/mod_icon.png",
+	version = "0.1.0",
+	modApiVersion = "2.9.4",
+	gameVersion = "1.2.93",
+	dependencies = {
+        modApiExt = "1.23",
+        memedit = "1.2.0",
+        redactedrice_memhack = "0.1.0",
+    },
+}
+
+function mod:init(options)
+	-- todo
+end
+
+function mod:load(options, version)
+	-- TODO
+end
+
+return mod
