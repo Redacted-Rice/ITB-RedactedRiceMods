256d20bfbeaf23606272887049fb09de839b54f7
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2025-12-10 11:18:11 -0500

    First cut at fixing some things from the playthrough

--- RedactedRiceMods/mods/Treeherders/scripts/forestUtils.lua
+++ RedactedRiceMods/mods/Treeherders/scripts/forestUtils.lua
@@ -3,11 +3,13 @@ forestUtils = {}
 forestUtils.predictableRandom = mod_loader.mods[modApi.currentMod].libs.predictableRandom
 
 forestUtils.floraformBounce = -3
+forestUtils.soundGrow = "/impact/generic/explosion"
+forestUtils.soundDamage = "/impact/generic/explosion"
 
 function forestUtils.arrayLength(array)
 	local count = 0
-	for _, _ in pairs(array) do 
-		count = count + 1 
+	for _, _ in pairs(array) do
+		count = count + 1
 	end
 	return count
 end
@@ -24,7 +26,7 @@ function forestUtils.isAVek(pawn)
 				isVek = false
 				break
 			end
-		end	
+		end
 	end
 	return isVek
 end
@@ -36,10 +38,10 @@ function forestUtils.isAForestFire(p)
 	--do special logic for the tip since we cannot use getTileFireType
 	if modapiext.weapon:isTipImage() then
 		return Board:IsFire(p)
-		
+
 	--normal logic
 	else
-		return Board:IsForestFire(p) or 
+		return Board:IsForestFire(p) or
 				(Board:GetTerrain(p) == TERRAIN_FOREST and Board:IsFire(p))
 	end
 end
@@ -63,50 +65,53 @@ end
 function forestUtils:getSpaceDamageWithoutSettingFire(p, damage, pushDir, allyImmune, buildingImmune)
 	local spaceDamage = nil
 	local pawn = Board:GetPawn(p)
-	
+
 	local sDamage = damage
 	if sDamage == 0 or (allyImmune and ((pawn and pawn:IsPlayer()) or Board:IsPod(p))) or (buildingImmune and Board:IsBuilding(p)) then
 		sDamage = DAMAGE_ZERO
 	end
-	
+
 	if pushDir then
 		spaceDamage = SpaceDamage(p, sDamage, pushDir)
 	else
 		spaceDamage = SpaceDamage(p, sDamage)
 	end
-	
+
 	--prevent it from setting a fire but don't put out an on fire pawn
 	if not (Board:IsFire(p) or (pawn and pawn:IsFire())) then
 		spaceDamage.iFire = EFFECT_REMOVE
 	end
-	
+
 	-- For some reason we AE, we have to reset if its a forest or else
 	-- the forest will disappear (but not forest fires)
 	if Board:GetTerrain(p) == TERRAIN_FOREST then
 		spaceDamage.iTerrain = TERRAIN_FOREST
 	end
-	
+
 	--cover up the forest fire icon
 	if sDamage > 0 and sDamage ~= DAMAGE_ZERO and Board:GetTerrain(p) == TERRAIN_FOREST and not Board:IsFire(p) then
 		spaceDamage.sImageMark = "combat/icons/icon_th_forest_burn_cover.png"
 	end
-	
+
 	return spaceDamage
 end
 
 function forestUtils:getFloraformSpaceDamage(p, damage, pushDir, allyImmune, buildingImmune)
 	local spaceDamage = self:getSpaceDamageWithoutSettingFire(p, damage, pushDir, allyImmune, buildingImmune)
-	
+
 	if self.isSpaceFloraformable(p) and not self.isAForest(p) then
 		spaceDamage.iTerrain = TERRAIN_FOREST
 		spaceDamage.sImageMark = "combat/icons/damage_floraform.png"
-		
+		spaceDamage.sSound = self.soundGrow
+
 		--This should prevent tile that are on fire from becoming fire tiles with forests instead of forest fire tiles
 		if Board:IsFire(p) then
 			spaceDamage.iFire = EFFECT_CREATE
 		end
+	else
+		spaceDamage.sSound = self.soundDamage
 	end
-	
+
 	return spaceDamage
 end
 
@@ -114,41 +119,41 @@ end
 
 function forestUtils:getNumForestsInPlus(p)
 	local count = 0
-	
+
 	if self.isAForest(p) then
 		count = count + 1
 	end
-	
+
 	for dir = 0, 3 do
 		if forestUtils.isAForest(p + DIR_VECTORS[dir]) then
 			count = count + 1
 		end
 	end
-	
+
 	return count
 end
 
 function forestUtils:floraformSpace(effect, point, damage, pushDir, allyImmune, buildingImmune)
 	damage = damage or DAMAGE_ZERO
-	
-	effect:AddDamage(self:getFloraformSpaceDamage(point, damage, pushDir, allyImmune, buildingImmune))			
+
+	effect:AddDamage(self:getFloraformSpaceDamage(point, damage, pushDir, allyImmune, buildingImmune))
 	effect:AddBounce(point, self.floraformBounce)
 end
 
 local function randFloraformDamage(effect, point, damage, pushDir, damageOnlyEnemy, allyImmune, buildingImmune)
 	local pawn = Board:GetPawn(point)
-	
+
 	if damageOnlyEnemy and not (pawn and pawn:IsEnemy()) then
 		damage = DAMAGE_ZERO
 	end
-	
+
 	forestUtils:floraformSpace(effect, point, damage, pushDir, allyImmune, buildingImmune)
 end
 
 function forestUtils:floraformNumOfRandomSpaces(effect, randId, candidates, numToForm, damage, pushDir,
 					damageOnlyEnemy, allyImmune, buildingImmune, seekMech, seekEnemy, randSalt, skipFloraformableCheck)
 	damage = damage or DAMAGE_ZERO
-	
+
 	--remove any points that can't be floraformed
 	if not skipFloraformableCheck then
 		local toRemove = {}
@@ -158,40 +163,40 @@ function forestUtils:floraformNumOfRandomSpaces(effect, randId, candidates, numT
 				table.insert(toRemove, k)
 			end
 		end
-		
+
 		--do it in two loops to ensure the iterator isn't messed up by removing items mid iteration
 		for _, v in pairs(toRemove) do
 			candidates[v] = nil
 		end
 	end
-	
+
 	--if there are not enough, floraform them all and return how many were floraformed
 	local retList = {}
 	if forestUtils.arrayLength(candidates) <= numToForm then
-		for k, point in pairs(candidates) do	
+		for k, point in pairs(candidates) do
 			randFloraformDamage(effect, point, damage, pushDir, damageOnlyEnemy, allyImmune, buildingImmune)
 			retList[k] = point
 		end
-		
+
 	--if there are enough points choose some random ones
 	else
 		local leftToFloraForm = numToForm
-		
+
 		local preferred = {}
-		if seekMech or seekEnemy then	
+		if seekMech or seekEnemy then
 			for k, v in pairs(candidates) do
 				local unit = Board:GetPawn(v)
 				if unit and ((seekEnemy and unit:IsEnemy()) or (seekMech and unit:IsMech())) then
 					preferred[k] = v
 				end
-			end		
-			
-			retList = self:floraformNumOfRandomSpaces(effect, randId, preferred, leftToFloraForm, 
+			end
+
+			retList = self:floraformNumOfRandomSpaces(effect, randId, preferred, leftToFloraForm,
 					damage, pushDir, damageOnlyEnemy, allyImmune, buildingImmune, false, false, randSalt, skipFloraformableCheck)
 			leftToFloraForm = leftToFloraForm - self.arrayLength(retList)
 		end
-	
-	
+
+
 		if leftToFloraForm > 0 then
 			local keys = {}
 			for k, _ in pairs(candidates) do
@@ -200,26 +205,26 @@ function forestUtils:floraformNumOfRandomSpaces(effect, randId, candidates, numT
 					table.insert(keys, k)
 				end
 			end
-			
+
 			for i = 1, leftToFloraForm do
-				if #keys > 0 then		
+				if #keys > 0 then
 					local index = forestUtils.predictableRandom:getNextValue(randId, 1, #keys, randSalt)
 					local point = candidates[keys[index]]
-					
+
 					retList[keys[index]] = point
 					candidates[keys[index]] = nil
 					table.remove(keys, index)
-					
+
 					randFloraformDamage(effect, point, damage, pushDir, damageOnlyEnemy, allyImmune, buildingImmune)
 				end
 			end
 		end
 	end
-	
-	
+
+
 	--Reset the roll at the end to keep the state clean in case this is rebuilt
 	forestUtils.predictableRandom:resetToLastRoll(randId)
-	
+
 	return retList
 end
 
@@ -248,9 +253,9 @@ end
 
 function forestUtils:getSpaces(predicate, ...)
 	assert(type(predicate) == "function")
-	
+
 	local matches = {}
-	
+
 	if Board then
 		--go through each space on the board and check if it matches
 		local size = Board:GetSize()
@@ -263,7 +268,7 @@ function forestUtils:getSpaces(predicate, ...)
 			end
 		end
 	end
-	
+
 	return matches
 end
 
@@ -278,7 +283,7 @@ function forestUtils:getSpacesThatBorder(predicate, ...)
 		for y = 0, size.y - 1 do
 			for x = 0, size.x - 1 do
 				local currPoint = Point(x, y)
-				
+
 				--only check + x and y spaces to optimize some
 				local adjacents = {}
 				if x < size.x - 1 then
@@ -287,7 +292,7 @@ function forestUtils:getSpacesThatBorder(predicate, ...)
 				if y < size.y - 1 then
 					adjacents[self:getSpaceHash(x, y + 1)] = Point(x, y + 1)
 				end
-				
+
 				--if it matches then see if we need to add the + x and y spaces
 				if predicate(currPoint, ...) then
 					for hash, point in pairs(adjacents) do
@@ -307,14 +312,14 @@ function forestUtils:getSpacesThatBorder(predicate, ...)
 			end
 		end
 	end
-	
+
 	return bordering
 end
 
 local function getGrouping_internal(space, groupedSpaces, boarderingSpaces, predicate, ...)
 	if not groupedSpaces[forestUtils:getSpaceHash(space)] then
 		groupedSpaces[forestUtils:getSpaceHash(space)] = space
-	
+
 		local size = Board:GetSize()
 		for _, dir in pairs(DIR_VECTORS) do
 			local p = space + dir
@@ -331,27 +336,27 @@ end
 
 function forestUtils:getGroupingOfSpaces(spaceInGroup, predicate, ...)
 	assert(type(predicate) == "function")
-	
+
 	local spaces = {}
 	spaces.group = {}
 	spaces.boardering = {}
-	
+
 	getGrouping_internal(spaceInGroup, spaces.group, spaces.boardering, predicate, ...)
-	
+
 	return spaces
 end
 
 function forestUtils:getGroupingOfSpacesMultiple(spacesInGroup, predicate, ...)
 	assert(type(predicate) == "function")
-	
+
 	local spaces = {}
 	spaces.group = {}
 	spaces.boardering = {}
-	
+
 	for _, space in pairs(spacesInGroup) do
 		getGrouping_internal(space, spaces.group, spaces.boardering, predicate, ...)
 	end
-	
+
 	return spaces
 end
 
@@ -363,23 +368,23 @@ function forestUtils:isSpaceSurroundedBy(space, predicate, ...)
 			space + DIR_VECTORS[3],
 			space + DIR_VECTORS[0],
 	}
- 
-	for _, p in pairs(surrondingTiles) do 
+
+	for _, p in pairs(surrondingTiles) do
 		if not predicate(p, ...) then
 			return false
 		end
 	end
-	
+
 	return true
 end
 
 function forestUtils:getClosestOfSpaces(space, otherSpaces)
 	assert(type(otherSpaces) == "table")
-	
+
 	local closest = nil
 	local closestDistance = 999 --much larger than should ever exist
 	local dist = 0
-	
+
 	for _, s in pairs(otherSpaces) do
 		--Board:GetDistance() doesn't seem to work consistently so do it ourselves
 		dist = math.abs(space.x - s.x) +  math.abs(space.y - s.y)
@@ -388,7 +393,7 @@ function forestUtils:getClosestOfSpaces(space, otherSpaces)
 			closest = s
 		end
 	end
-	
+
 	return closest
 end
 
@@ -407,5 +412,5 @@ end
 
 
 return forestUtils
-    
-    
+
+
--- RedactedRiceMods/mods/Treeherders/scripts/weapons/th_forestfire.lua
+++ RedactedRiceMods/mods/Treeherders/scripts/weapons/th_forestfire.lua
@@ -6,27 +6,28 @@ Treeherders_ForestFire = Skill:new{
 	Rarity = 1,
 	Damage = 1,
 	PowerCost = 1,
-	LaunchSound = "/weapons/artillery_volley",
-	ImpactSound = "/impact/generic/explosion",	
+	--LaunchSound = "/weapons/artillery_volley",
+	ShootSound = "/weapons/artillery_volley",
+	ImpactSound = "/impact/generic/explosion",
 	UpShot = "effects/shotup_th_deadtree.png",
 	UpShotOuter = "effects/shotup_th_deadtree.png",
 	Explosion = "",
 	BounceAmount = forestUtils.floraformBounce,
 	Upgrades = 2,
 	UpgradeCost = { 1, 3 },
-	
+
 	TwoClick = true,
-	
+
 	-- Range
 	ArtilleryStart = 2,
 	ArtillerySize = 5,
-	
+
 	-- Custom
 	PushOuter = false,
 	DamageOuter = 0,
 	BounceOuterAmount = 2,
 	BuildingDamage = true,
-	
+
 	--TipImage
     TipImage = {
 		Unit = Point(3,3),
@@ -63,10 +64,10 @@ Treeherders_ForestFire_AB = Treeherders_ForestFire_B:new
 -- First action is to move to any space in the forest
 function Treeherders_ForestFire:GetTargetArea(point)
 	local ret = PointList()
-	
+
 	-- always can just not move
 	ret:push_back(point)
-	
+
 	-- if we are on a forest then return the point we can move to in that forest
 	-- this is needed with how getGroupingOfSpaces works since it consideres the
 	-- point to be of the right type or as part of the boarder
@@ -79,14 +80,13 @@ function Treeherders_ForestFire:GetTargetArea(point)
 				ret:push_back(Point(v))
 			end
 		end
-	end 
+	end
 	return ret
 end
 
 function Treeherders_ForestFire:GetSkillEffect(p1, p2)
 	local ret = SkillEffect()
 	if p1 ~= p2 then
-		-- TODO: Make cool move effect like Seismic Beast's Amonzite Drill
 		ret:AddTeleport(p1,p2,FULL_DELAY)
 	else
 		ret:AddDamage(SpaceDamage(p1,0))
@@ -96,7 +96,7 @@ end
 
 function Treeherders_ForestFire:GetSecondTargetArea(p1,p2)
 	local ret = PointList()
-	
+
 	-- Since we aren't inheriting from the artillery class,
 	-- we reimplement the logic for our need
 	for dir = DIR_START, DIR_END do
@@ -108,7 +108,7 @@ function Treeherders_ForestFire:GetSecondTargetArea(p1,p2)
 			ret:push_back(curr)
 		end
 	end
-	
+
 	return ret
 end
 
@@ -116,42 +116,43 @@ function Treeherders_ForestFire:GetFinalEffect(p1,p2,p3)
 	-- Start with the previous partial effect, add a pause and build on it
 	local ret = self:GetSkillEffect(p1, p2)
 	ret:AddDelay(0.2)
-	
+
 	local attackDir = GetDirection(p3 - p2)
-	
+
 	local pBack = p2 + DIR_VECTORS[(attackDir + 2) % 4]
 	if Board:IsValid(pBack) and forestUtils.isSpaceFloraformable(pBack) then
 		forestUtils:floraformSpace(ret, pBack)
 	end
-		
+
 	local damage = forestUtils:getFloraformSpaceDamage(p3, self.Damage, attackDir, false, not self.BuildingDamage)
 	ret:AddBounce(p2, 1)
-	
+
 	local mainDelay = FULL_DELAY
 	if self.DamageOuter > 0 then
 		mainDelay = 0
 	end
+	damage.sSound = self.SoundShoot
 	ret:AddArtillery(p2, damage, self.UpShot, mainDelay)
 	ret:AddBounce(p3, 1)
-	
+
 	if self.DamageOuter > 0 then
 		dir1 = (attackDir + 1) % 4
 		dir2 = (attackDir - 1) % 4
-		
+
 		local side1 = p3 + DIR_VECTORS[dir1]
 		local side2 = p3 + DIR_VECTORS[dir2]
-		
+
 		local side1Damage = forestUtils:getSpaceDamageWithoutSettingFire(side1, self.DamageOuter, attackDir, false, not self.BuildingDamage)
 		local side2Damage = forestUtils:getSpaceDamageWithoutSettingFire(side2, self.DamageOuter, attackDir, false, not self.BuildingDamage)
 
 		ret:AddArtillery(p2, side1Damage, self.UpShotOuter, 0)
 		ret:AddArtillery(p2, side2Damage, self.UpShotOuter, FULL_DELAY)
-		
-		if self.BounceOuterAmount ~= 0 then	
-			ret:AddBounce(side1, self.BounceOuterAmount) 
-			ret:AddBounce(side2, self.BounceOuterAmount) 
+
+		if self.BounceOuterAmount ~= 0 then
+			ret:AddBounce(side1, self.BounceOuterAmount)
+			ret:AddBounce(side2, self.BounceOuterAmount)
 		end
 	end
-	
+
 	return ret
 end
