6f45424af8f1eccd8908687922d7df361ca464c5
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2025-12-27 16:00:28 -0500

    Clarifying some weapon texts and first part of making entborg a cyborg

--- RedactedRiceMods/mods/Treeherders/scripts/weapons/th_violentgrowth.lua
+++ RedactedRiceMods/mods/Treeherders/scripts/weapons/th_violentgrowth.lua
@@ -2,7 +2,7 @@ Treeherders_ViolentGrowth = Skill:new
 {
 	Name = "Violent Growth",
     Class = "Science",
-    Description = "Grows a forest in an unforested tile, damaging the tile, otherwise cancels target's attack. Repeat on a second tile in or next to the targeted forest",
+    Description = "If targetting an unforested tile, grows a forest tile causing damage. Otherwise cancels target's attack. Repeat on a second tile in or next to the targeted forest. Does not start fires.",
 	Icon = "weapons/science_th_violentGrowth.png",
 	Rarity = 1,
 
