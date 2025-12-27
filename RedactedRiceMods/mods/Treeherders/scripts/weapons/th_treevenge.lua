89b6bd719ee662775103dc45097e98347b77cb98
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2025-12-27 16:00:28 -0500

    Clarifying some weapon texts and first part of making entborg a cyborg

--- RedactedRiceMods/mods/Treeherders/scripts/weapons/th_treevenge.lua
+++ RedactedRiceMods/mods/Treeherders/scripts/weapons/th_treevenge.lua
@@ -1,8 +1,8 @@
 Treeherders_Treevenge = Skill:new
 {
 	Name = "Tree-venge",
-	Class = "Prime",
-	Description = "Smash an adjacent tile pushing surrounding tiles and damaging them by half. Target damage increases by forest fires / 2 (rounded up) to a max of 5",
+	Class = "TechnoVek",
+	Description = "Smash an adjacent tile pushing surrounding tiles and damaging them by half. Target damage increases by forest fires / 2 (rounded up) to a max of 5. Does not start fires.",
 	Icon = "weapons/prime_th_treevenge.png",
 	Rarity = 2,
 	
