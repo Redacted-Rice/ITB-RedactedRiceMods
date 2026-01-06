c51f20785ae85a60e5f3c51b98e98a87f8f4a733
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-01-06 17:32:43 -0500

    Adding rebalance for hp & move to distinguish from skilled

--- RedactedRiceMods/mods/GridDef+/scripts/init.lua
+++ RedactedRiceMods/mods/GridDef+/scripts/init.lua
@@ -1,6 +1,6 @@
 local mod = {
-	id = "redactedrice_griddefplus",
-	name = "+3 -> +8 Grid DEF",
+	id = "redactedrice_vanillaplus",
+	name = "AE Rebalance Base Skills",
 	icon = "mod_icon.png",
 	version = "1.0.0",
 	modApiVersion = "2.9.4",
@@ -9,10 +9,18 @@ local mod = {
         redactedrice_memhack = "0.5.0", -- TBD
         redactedrice_cplus_plus = "0.5.0", -- TBD
     },
-	cplusCategory = "RrGridPlus",
-	GridPlus = {id = "GridPlus", shortName = "+8 Grid DEF", fullName = "Pilot_GridName", 
-			description = "Grid Defense increased by 8. This affects the chance of resisting Building Damage in combat.", 
-			bonuses = {grid = 8}, saveVal = 2 
+	cplusCategory = "RrVanillaPlus",
+	HealthPlus = {id = "HealthPlus", shortName = "Health+", fullName = "Pilot_HealthName",
+			description = "Increase Health by 3.",
+			bonuses = {health = 3}, saveVal = 0, reusability = "reusable"
+	},
+	MovePlus = {id = "MovePlus", shortName = "Move+", fullName = "Pilot_MoveName",
+			description = "Increase Move by 1 and Grid DEF by 4.",
+			bonuses = {move = 1, grid = 4}, saveVal = 0, reusability = "reusable"
+	},
+	GridPlus = {id = "GridPlus", shortName = "Grid DEF+", fullName = "Pilot_GridName",
+			description = "Grid Defense increased by 8",
+			bonuses = {grid = 8}, saveVal = 2, reusability = "reusable"
 	},
 }
 
@@ -21,9 +29,11 @@ function mod:init()
 end
 
 function mod:load(options, version)
-	-- Replace grid with increased grid
+	-- Replace vanilla skills with slighlty better ones
+	cplus_plus_ex.plus_manager:disableSkill("Health")
+	cplus_plus_ex.plus_manager:disableSkill("Move")
 	cplus_plus_ex.plus_manager:disableSkill("Grid")
-	-- Just enable by category even though we have only one
+
 	cplus_plus_ex.plus_manager:enableCategory(self.cplusCategory)
 end
 
