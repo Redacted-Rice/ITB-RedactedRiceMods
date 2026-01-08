9ac58bfc22b997abe9e04ef97704037b4de2dd3f
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-01-08 17:31:15 -0500

    Updating for latest C++ Ex changes

--- RedactedRiceMods/mods/GridDef+/scripts/init.lua
+++ RedactedRiceMods/mods/GridDef+/scripts/init.lua
@@ -12,29 +12,30 @@ local mod = {
 	cplusCategory = "RrVanillaPlus",
 	HealthPlus = {id = "HealthPlus", shortName = "Health+", fullName = "Pilot_HealthName",
 			description = "Increase Health by 3.",
-			bonuses = {health = 3}, saveVal = 0, reusability = "reusable"
+			bonuses = {health = 3}, saveVal = 0, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
 	},
 	MovePlus = {id = "MovePlus", shortName = "Move+", fullName = "Pilot_MoveName",
 			description = "Increase Move by 1 and Grid DEF by 4.",
-			bonuses = {move = 1, grid = 4}, saveVal = 0, reusability = "reusable"
+			bonuses = {move = 1, grid = 4}, saveVal = 1, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
 	},
 	GridPlus = {id = "GridPlus", shortName = "Grid DEF+", fullName = "Pilot_GridName",
 			description = "Grid Defense increased by 8",
-			bonuses = {grid = 8}, saveVal = 2, reusability = "reusable"
+			bonuses = {grid = 8}, saveVal = 2, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
 	},
 }
 
 function mod:init()
-	cplus_plus_ex.plus_manager:registerSkill(self.cplusCategory, self.GridPlus)
+	-- register on init
+	cplus_plus_ex:registerSkill(self.cplusCategory, self.HealthPlus)
+	cplus_plus_ex:registerSkill(self.cplusCategory, self.MovePlus)
+	cplus_plus_ex:registerSkill(self.cplusCategory, self.GridPlus)
 end
 
 function mod:load(options, version)
-	-- Replace vanilla skills with slighlty better ones
-	cplus_plus_ex.plus_manager:disableSkill("Health")
-	cplus_plus_ex.plus_manager:disableSkill("Move")
-	cplus_plus_ex.plus_manager:disableSkill("Grid")
-
-	cplus_plus_ex.plus_manager:enableCategory(self.cplusCategory)
+	-- Do config changes on load
+	cplus_plus_ex:disableSkill("Health")
+	cplus_plus_ex:disableSkill("Move")
+	cplus_plus_ex:disableSkill("Grid")
 end
 
 return mod
