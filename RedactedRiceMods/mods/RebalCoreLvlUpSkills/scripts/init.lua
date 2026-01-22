a113d4ac654a07ba18992fc9a1532093848855b1
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-01-21 19:29:54 -0500

    Revert "Fixing loading. Apparantly you can't have any additional fields in your mod"
    This reverts commit 183a8a62c323bec8e61ff0147b82a86cc216abcd.

--- RedactedRiceMods/mods/RebalCoreLvlUpSkills/scripts/init.lua
+++ RedactedRiceMods/mods/RebalCoreLvlUpSkills/scripts/init.lua
@@ -1,40 +1,36 @@
 local mod = {
 	id = "redactedrice_RebalCorePlus",
 	name = "Rebalanced Core Lvl Up Skills",
+	description = "Rebalances the core (none AE) level up skills to be more inline with AE skills",
 	icon = "mod_icon.png",
 	version = "1.0.0",
 	modApiVersion = "2.9.4",
 	gameVersion = "1.2.93",
 	dependencies = {
-        redactedrice_memhack = "0.1.0", -- TBD
-        redactedrice_cplus_plus = "0.1.0", -- TBD
-    }
+        redactedrice_memhack = "0.5.0", -- TBD
+        redactedrice_cplus_plus = "0.5.0", -- TBD
+    },
+	cplusCategory = "RrVanillaPlus",
+	-- TODO: Align description with vanilla texts
+	HealthPlus = {id = "HealthPlus", shortName = "Health+", fullName = "Pilot_HealthName",
+			description = "Increase Health by 3.",
+			bonuses = {health = 3}, saveVal = 0, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
+	},
+	MovePlus = {id = "MovePlus", shortName = "Move+", fullName = "Pilot_MoveName",
+			description = "Increase Move by 1 and Grid DEF by 4.",
+			bonuses = {move = 1, grid = 4}, saveVal = 1, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
+	},
+	GridPlus = {id = "GridPlus", shortName = "Grid DEF+", fullName = "Pilot_GridName",
+			description = "Grid Defense increased by 8",
+			bonuses = {grid = 8}, saveVal = 2, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
+	},
 }
 
 function mod:init()
-	local cplusCategory = "RrVanillaPlus"
-	-- TODO: Align description with vanilla texts
-	local healthPlus = {id = "HealthPlus", shortName = "Health+", fullName = "Pilot_HealthName",
-			description = "Increase Health by 3.",
-			bonuses = {health = 3}, saveVal = 0, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
-	}
-	local movePlus = {id = "MovePlus", shortName = "Move+", fullName = "Pilot_MoveName",
-			description = "Increase Move by 1 and Grid DEF by 4.",
-			bonuses = {move = 1, grid = 4}, saveVal = 1, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
-	}
-	local gridPlus = {id = "GridPlus", shortName = "Grid DEF+", fullName = "Pilot_GridName",
-			description = "Grid Defense increased by 8",
-			bonuses = {grid = 8}, saveVal = 2, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
-	}
-	
 	-- register on init
-	cplus_plus_ex:registerSkill(cplusCategory, healthPlus)
-	cplus_plus_ex:registerSkill(cplusCategory, movePlus)
-	cplus_plus_ex:registerSkill(cplusCategory, gridPlus)
-	
-	-- Respect the vanilla health exclusions
-	cplus_plus_ex:registerPilotSkillExclusions("Pilot_Rock", healthPlus)
-	cplus_plus_ex:registerPilotSkillExclusions("Pilot_Zoltan", healthPlus)
+	cplus_plus_ex:registerSkill(self.cplusCategory, self.HealthPlus)
+	cplus_plus_ex:registerSkill(self.cplusCategory, self.MovePlus)
+	cplus_plus_ex:registerSkill(self.cplusCategory, self.GridPlus)
 end
 
 function mod:load(options, version)
@@ -42,7 +38,6 @@ function mod:load(options, version)
 	cplus_plus_ex:disableSkill("Health")
 	cplus_plus_ex:disableSkill("Move")
 	cplus_plus_ex:disableSkill("Grid")
-	
 end
 
 return mod
