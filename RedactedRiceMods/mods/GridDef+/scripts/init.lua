36d7ba23ff770f8ecfca8cc31148217b0ea33756
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-01-21 19:29:54 -0500

    Revert "Updating for latest C++ Ex changes"
    This reverts commit 0bd94ba1d296ec6181bc9b686c7be24fd171ad7f.

--- RedactedRiceMods/mods/GridDef+/scripts/init.lua
+++ RedactedRiceMods/mods/GridDef+/scripts/init.lua
@@ -12,30 +12,29 @@ local mod = {
 	cplusCategory = "RrVanillaPlus",
 	HealthPlus = {id = "HealthPlus", shortName = "Health+", fullName = "Pilot_HealthName",
 			description = "Increase Health by 3.",
-			bonuses = {health = 3}, saveVal = 0, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
+			bonuses = {health = 3}, saveVal = 0, reusability = "reusable"
 	},
 	MovePlus = {id = "MovePlus", shortName = "Move+", fullName = "Pilot_MoveName",
 			description = "Increase Move by 1 and Grid DEF by 4.",
-			bonuses = {move = 1, grid = 4}, saveVal = 1, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
+			bonuses = {move = 1, grid = 4}, saveVal = 0, reusability = "reusable"
 	},
 	GridPlus = {id = "GridPlus", shortName = "Grid DEF+", fullName = "Pilot_GridName",
 			description = "Grid Defense increased by 8",
-			bonuses = {grid = 8}, saveVal = 2, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
+			bonuses = {grid = 8}, saveVal = 2, reusability = "reusable"
 	},
 }
 
 function mod:init()
-	-- register on init
-	cplus_plus_ex:registerSkill(self.cplusCategory, self.HealthPlus)
-	cplus_plus_ex:registerSkill(self.cplusCategory, self.MovePlus)
-	cplus_plus_ex:registerSkill(self.cplusCategory, self.GridPlus)
+	cplus_plus_ex.plus_manager:registerSkill(self.cplusCategory, self.GridPlus)
 end
 
 function mod:load(options, version)
-	-- Do config changes on load
-	cplus_plus_ex:disableSkill("Health")
-	cplus_plus_ex:disableSkill("Move")
-	cplus_plus_ex:disableSkill("Grid")
+	-- Replace vanilla skills with slighlty better ones
+	cplus_plus_ex.plus_manager:disableSkill("Health")
+	cplus_plus_ex.plus_manager:disableSkill("Move")
+	cplus_plus_ex.plus_manager:disableSkill("Grid")
+
+	cplus_plus_ex.plus_manager:enableCategory(self.cplusCategory)
 end
 
 return mod
