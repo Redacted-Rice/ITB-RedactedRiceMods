132dcbc72f760349c3acdc578213b1c30d8e0d84
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-01-21 19:29:54 -0500

    Revert "Changing to try and use setText to be more mod/localization friendly"
    This reverts commit 10160ba21ecd6d6b2cf05520edc894c1f5ec3ef5.

--- RedactedRiceMods/mods/RebalCoreLvlUpSkills/scripts/init.lua
+++ RedactedRiceMods/mods/RebalCoreLvlUpSkills/scripts/init.lua
@@ -12,57 +12,26 @@ local mod = {
 }
 
 function mod:init()
-	-- use the set text approach so other can override these if they want to for some reason
-	-- such as localization
-	modApi:setText("RebalCorePlus_HealthPlus_ShortName", "Health+")
-	modApi:setText("RebalCorePlus_HealthPlus_FullName", "Health +3")
-	modApi:setText("RebalCorePlus_HealthPlus_Description", "Increase Health by 3.")
-
-	modApi:setText("RebalCorePlus_MovePlus_ShortName", "Move+")
-	modApi:setText("RebalCorePlus_MovePlus_FullName", "Move +1, Grid DEF +4")
-	modApi:setText("RebalCorePlus_MovePlus_Description", "Increase Move by 1 and Grid DEF by 4.")
-
-	modApi:setText("RebalCorePlus_GridPlus_ShortName", "Grid DEF+")
-	modApi:setText("RebalCorePlus_GridPlus_FullName", "Grid DEF +8")
-	modApi:setText("RebalCorePlus_GridPlus_Description", "Grid Defense increased by 8")
-
 	local cplusCategory = "RrVanillaPlus"
-
-	local healthPlus = {
-		id = "HealthPlus",
-		shortName = "RebalCorePlus_HealthPlus_ShortName",
-		fullName = "RebalCorePlus_HealthPlus_FullName",
-		description = "RebalCorePlus_HealthPlus_Description",
-		bonuses = {health = 3},
-		saveVal = 0,
-		reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
+	-- TODO: Align description with vanilla texts
+	local healthPlus = {id = "HealthPlus", shortName = "Health+", fullName = "Pilot_HealthName",
+			description = "Increase Health by 3.",
+			bonuses = {health = 3}, saveVal = 0, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
 	}
-
-	local movePlus = {
-		id = "MovePlus",
-		shortName = "RebalCorePlus_MovePlus_ShortName",
-		fullName = "RebalCorePlus_MovePlus_FullName",
-		description = "RebalCorePlus_MovePlus_Description",
-		bonuses = {move = 1, grid = 4},
-		saveVal = 1,
-		reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
+	local movePlus = {id = "MovePlus", shortName = "Move+", fullName = "Pilot_MoveName",
+			description = "Increase Move by 1 and Grid DEF by 4.",
+			bonuses = {move = 1, grid = 4}, saveVal = 1, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
 	}
-
-	local gridPlus = {
-		id = "GridPlus",
-		shortName = "RebalCorePlus_GridPlus_ShortName",
-		fullName = "RebalCorePlus_GridPlus_FullName",
-		description = "RebalCorePlus_GridPlus_Description",
-		bonuses = {grid = 8},
-		saveVal = 2,
-		reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
+	local gridPlus = {id = "GridPlus", shortName = "Grid DEF+", fullName = "Pilot_GridName",
+			description = "Grid Defense increased by 8",
+			bonuses = {grid = 8}, saveVal = 2, reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
 	}
-
+	
 	-- register on init
 	cplus_plus_ex:registerSkill(cplusCategory, healthPlus)
 	cplus_plus_ex:registerSkill(cplusCategory, movePlus)
 	cplus_plus_ex:registerSkill(cplusCategory, gridPlus)
-
+	
 	-- Respect the vanilla health exclusions
 	cplus_plus_ex:registerPilotSkillExclusions("Pilot_Rock", healthPlus)
 	cplus_plus_ex:registerPilotSkillExclusions("Pilot_Zoltan", healthPlus)
@@ -73,7 +42,7 @@ function mod:load(options, version)
 	cplus_plus_ex:disableSkill("Health")
 	cplus_plus_ex:disableSkill("Move")
 	cplus_plus_ex:disableSkill("Grid")
-
+	
 end
 
 return mod
