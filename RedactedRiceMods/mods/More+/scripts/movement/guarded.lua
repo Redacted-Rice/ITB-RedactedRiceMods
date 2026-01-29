f3efb61c23607c2e62c4cb34cfe00711c3968b0f
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-01-29 08:43:39 -0500

    Some fixes to existing skills and adding hyper

--- RedactedRiceMods/mods/More+/scripts/movement/guarded.lua
+++ RedactedRiceMods/mods/More+/scripts/movement/guarded.lua
@@ -1,21 +1,32 @@
 local skill = {
 	id = "RrGuarded",
 	name = "Guarded",
-	desc = "Mech is guarding and cannot be pushed"
-	reusability = cplus_plus_ex.REUSABLILITY.PER_PILOT
+	desc = "Mech is guarding and cannot be pushed",
+	reusability = cplus_plus_ex.REUSABLILITY.PER_PILOT,
+	modified = {}
 }
 
 -- no init needed
 
 function skill:load() 
 	if cplus_plus_ex.isSkillEnabled(self.id) then
-		cplus_plus_ex:addSkillActiveHook(self.applyGuarding)
+		cplus_plus_ex:addSkillActiveHook(self.applyEffect)
 	end
 end
 
-function skill.applyGuarding(skillId, isActive, pawnId, pilot, skill)
+function skill.applyEffect(skillId, isActive, pawnId, pilot, skill)
 	if skillId == skill.id then
-		Game:GetPawn(pawnId):SetPushable(not isActive)
+		local pawn = Game:GetPawn(pawnId)
+		if isActive then
+			if not pawn:IsGuarding() then
+				Game:GetPawn(pawnId):SetPushable(true)
+				modified[pawnId] = pawn
+			end
+		else
+			if modified[pawnId] then
+				Game:GetPawn(pawnId):SetPushable(false)
+			end
+		end
 	end
 end
 
