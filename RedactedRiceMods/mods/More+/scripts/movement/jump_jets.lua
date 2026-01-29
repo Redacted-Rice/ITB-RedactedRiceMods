900db9088a9c7d0b8618fe9c6170fdaa9f460986
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-01-29 08:43:39 -0500

    Some fixes to existing skills and adding hyper

--- RedactedRiceMods/mods/More+/scripts/movement/jump_jets.lua
+++ RedactedRiceMods/mods/More+/scripts/movement/jump_jets.lua
@@ -1,21 +1,32 @@
 local skill = {
 	id = "RrJumpJets",
 	name = "Jump Jets",
-	desc = "Mech uses jump jets to leap instead of typical movement"
-	reusability = cplus_plus_ex.REUSABLILITY.PER_PILOT
+	desc = "Mech uses jump jets to leap instead of typical movement",
+	reusability = cplus_plus_ex.REUSABLILITY.PER_PILOT,
+	modified = {},
 }
 
 -- no init needed
 
 function skill:load() 
 	if cplus_plus_ex.isSkillEnabled(self.id) then
-		cplus_plus_ex:addSkillActiveHook(self.applyJumper)
+		cplus_plus_ex:addSkillActiveHook(self.applyEffect)
 	end
 end
 
-function skill.applyJumper(skillId, isActive, pawnId, pilot, skill)
+function skill.applyEffect(skillId, isActive, pawnId, pilot, skill)
 	if skillId == skill.id then
-		Game:GetPawn(pawnId):SetJumper(isActive)
+		local pawn = Game:GetPawn(pawnId)
+		if isActive then
+			if not pawn:IsJumper() then
+				Game:GetPawn(pawnId):SetJumper(true)
+				modified[pawnId] = pawn
+			end
+		else
+			if modified[pawnId] then
+				Game:GetPawn(pawnId):SetJumper(false)
+			end
+		end
 	end
 end
 
