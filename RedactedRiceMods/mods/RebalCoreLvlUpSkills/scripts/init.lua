a17e54f0b94bdd5211942b2d37abbe6f2fb70e01
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-02-02 23:38:12 -0500

    Adding option for configuring grid def value

--- RedactedRiceMods/mods/RebalCoreLvlUpSkills/scripts/init.lua
+++ RedactedRiceMods/mods/RebalCoreLvlUpSkills/scripts/init.lua
@@ -10,7 +10,10 @@ local mod = {
     }
 }
 
-function mod:init()
+function mod:init(options)
+	gridDef = options["rr_vplus_grid"].value
+	local halfRounded = math.floor(gridDef / 2.0)
+	
 	-- use the set text approach so other can override these if they want to for some reason
 	-- such as localization
 	modApi:setText("RebalCorePlus_HealthPlus_ShortName", "Health+")
@@ -18,12 +21,12 @@ function mod:init()
 	modApi:setText("RebalCorePlus_HealthPlus_Description", "Piloted Mech health is increased by 3.")
 
 	modApi:setText("RebalCorePlus_MovePlus_ShortName", "Move+")
-	modApi:setText("RebalCorePlus_MovePlus_FullName", "Move +1, Grid DEF +4")
-	modApi:setText("RebalCorePlus_MovePlus_Description", "Piloted Mech movement is increased by 1 and Grid Defense increased by 4.")
+	modApi:setText("RebalCorePlus_MovePlus_FullName", "Move +1, Grid DEF +"..halfRounded)
+	modApi:setText("RebalCorePlus_MovePlus_Description", "Piloted Mech movement is increased by 1 and Grid Defense increased by "..halfRounded..".")
 
 	modApi:setText("RebalCorePlus_GridPlus_ShortName", "Grid DEF+")
-	modApi:setText("RebalCorePlus_GridPlus_FullName", "Grid DEF +8")
-	modApi:setText("RebalCorePlus_GridPlus_Description", "Grid Defense increased by 8.")
+	modApi:setText("RebalCorePlus_GridPlus_FullName", "Grid DEF +"..gridDef)
+	modApi:setText("RebalCorePlus_GridPlus_Description", "Grid Defense increased by "..gridDef..".")
 
 	local cplusCategory = "Vanilla+"
 
@@ -42,7 +45,7 @@ function mod:init()
 		shortName = "RebalCorePlus_MovePlus_ShortName",
 		fullName = "RebalCorePlus_MovePlus_FullName",
 		description = "RebalCorePlus_MovePlus_Description",
-		bonuses = {move = 1, grid = 4},
+		bonuses = {move = 1, grid = halfRounded},
 		saveVal = 1,
 		reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
 	}
@@ -52,7 +55,7 @@ function mod:init()
 		shortName = "RebalCorePlus_GridPlus_ShortName",
 		fullName = "RebalCorePlus_GridPlus_FullName",
 		description = "RebalCorePlus_GridPlus_Description",
-		bonuses = {grid = 8},
+		bonuses = {grid = gridDef},
 		saveVal = 2,
 		reusability = cplus_plus_ex.REUSABLILITY.REUSABLE
 	}
@@ -73,6 +76,21 @@ function mod:load(options, version)
 	cplus_plus_ex:disableSkill("Move")
 	cplus_plus_ex:disableSkill("Grid")
 
+end
+
+function mod:metadata()
+	local option_values = {
+		gridDef = {4, 5, 6, 7, 8, 9, 10, 11, 12},
+	}
+	
+	modApi:addGenerationOption(
+		"rr_vplus_grid", "Grid+ Increase Amount",
+		"Changes the Grid DEF % increas for the Grid+ skill and the Move+ skill. Grid DEF will use this value. Move+ will use this value / 2 (round down).\nREQUIRES RESTART TO TAKE EFFECT!",
+		{
+			values = option_values.gridDef,
+			value = 8
+		}
+	)
 end
 
 return mod
