22da5e11778745e45b6e07bd397caa341c70028a
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-02-05 17:40:19 -0500

    Fixing some issues, adding base classes and updating for additions/changes to CPLUS+

--- RedactedRiceMods/mods/More+/scripts/offensive/ignorant.lua
+++ RedactedRiceMods/mods/More+/scripts/offensive/ignorant.lua
@@ -1,70 +1,30 @@
-local skill = {
-	id = "RrIgnorant",
-	name = "Ignorant",
-	desc = "Pilot gains boosted each turn but loses 2 xp per kill",
-	reusability = cplus_plus_ex.REUSABLILITY.PER_PILOT,
-	events = {},
-}
+local customSkill = SkillActive.new(
+	"RrIgnorant",
+	"Ignorant",
+	"Pilot gains boosted each turn but loses 2 XP per kill",
+	cplus_plus_ex.REUSABLILITY.PER_PILOT
+)
 
--- no init needed
-
-function skill.clearEvents()
-	for _, event in pairs(skill.events) do
-		event:unsubscribe()
-	end
+function customSkill.setupEffect()
+	table.insert(customSkill.events, modapiext.events.onPawnKilled:subscribe(customSkill.killedPawn))
+	table.insert(customSkill.events, modapiext.events.onNextTurn:subscribe(customSkill.boostOnTurn))
 end
 
-function skill:load() 
-	self.clearEvents()
-	
-	if cplus_plus_ex.isSkillEnabled(self.id) then
-		cplus_plus_ex:addSkillActiveHook(self.setupEffect)
-	end
-end
-
-function skill.setupEffect(skillId, isActive, pawnId, pilot, skill)
-	if skillId == skill.id then
-		local nowActive = #cplus_plus_ex.getMechsWithSkill(skillId) > 0
-		local wasActive = cplus_plus_ex.isSkillActive(skillId)
-		if nowActive and not wasActive then
-			-- add events
-			table.insert(events, modapiext.events.onPawnKilled:subscribe(skill.killedPawn))
-			table.insert(events, modapiext.events.onSkillStart:subscribe(skill.setAttackingMech))
-			table.insert(events, modapiext.events.onFinalEffectStart:subscribe(skill.setAttackingMech))
-			table.insert(events, modapiext.events.onSkillEnd:subscribe(skill.unsetAttackingMech))
-			table.insert(events, modapiext.events.onFinalEffectend:subscribe(skill.unsetAttackingMech))
-			table.insert(events, modapiext.events.onNextTurn:subscribe(skill.boostOnTurn))
-		elseif not notActive and wasActive then
-			-- remove events
-			self.clearEvents()
+function customSkill.killedPawn(mission, pawn, killer)
+	if killer ~= nil and pawn:isEnemy() then
+		local pilot = killer:GetPilot()
+		if cplus_plus_ex.isSkillOnPilot(customSkill.id, pilot) then
+			pilot:addXp(-2)
 		end
 	end
 end
 
-local trackedMech
-
-function skill.setAttackingMech(mission, pawn)
-	if pawn:isMech() and pawn:IsSkillActive(skill.id) then
-		trackedMech = pawn
-	end
-end
-
-function skill.unsetAttackingMech()
-	trackedMech = nil
-end
-
-function skill.killedPawn(mission, pawn)
-	if trackedMech ~= nil and pawn:isEnemy() then
-		trackedMech:GetPilot():addXp(-2)
-	end
-end
-
-function skill.boostOnTurn()
+function customSkill.boostOnTurn()
 	if  Game:GetTeamTurn() == TEAM_PLAYER then
-		for _, mech in pairs(cplus_plus_ex.getMechsWithSkill(skillId)) do
+		for _, mech in pairs(cplus_plus_ex.getMechsWithSkill(customSkill.id)) do
 			mech:SetBoosted(true)
 		end
 	end
 end
 
-return skill
+return customSkill
