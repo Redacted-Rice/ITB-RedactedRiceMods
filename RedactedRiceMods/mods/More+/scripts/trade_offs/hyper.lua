4516984e4c13cd31ccca66ddbb22f82467a6c78a
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-02-06 08:47:22 -0500

    Fixing base classes to be more flexible and fixing some issues with loading logic

--- RedactedRiceMods/mods/More+/scripts/trade_offs/hyper.lua
+++ RedactedRiceMods/mods/More+/scripts/trade_offs/hyper.lua
@@ -1,13 +1,13 @@
 local BASE_MOVE = 3
 
-local customSkill = SkillActive.new(
-	"RrHyper",
-	"Hyper",
-	"+3 movement, lose 1 movement at the end of each turn (min +0)",
-	cplus_plus_ex.REUSABLILITY.REUSABLE
-)
-customSkill.bonuses = {move = BASE_MOVE}
-customSkill.modified = {}
+local customSkill = more_plus.SkillActive:new{
+	id = "RrHyper",
+	name = "Hyper",
+	description = "+3 movement, lose 1 movement at the end of each turn (min +0)",
+	reusability = cplus_plus_ex.REUSABLILITY.REUSABLE,
+	bonuses = {move = BASE_MOVE},
+	modified = {}
+}
 
 function customSkill.setupEffect()
 	table.insert(customSkill.events, modapiext.events.onNextTurn:subscribe(customSkill.decreaseMove))
