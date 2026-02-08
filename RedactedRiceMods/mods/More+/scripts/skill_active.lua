2ba1db3b2e22cffde0ed72c96081bf23adc16a92
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-02-07 22:26:37 -0500

    More fixes and renaming. Traits at least are working now

--- RedactedRiceMods/mods/More+/scripts/skill_active.lua
+++ RedactedRiceMods/mods/More+/scripts/skill_active.lua
@@ -1,62 +1,46 @@
+local Skills = {}
+
 local SkillActive = {
 	events = {}
 }
 SkillActive.__index = SkillActive
 
---[[ TODO: take table instead
-function CreateClass(newclass)
-	local members = {}
-	
-	for i,v in pairs(newclass) do
-		members[#members + 1] = i 
-	end
-	
-	for i = 1, #members do
-		newclass["Get" .. members[i] ] = function (self, pawn) return self[members[i] ] end
-	end
-	
-	newclass.new = 	function(self,o)
-						o = o or {} -- create table if user does not provide one
-						setmetatable(o, self)
-						self.__index = self
-						return o
-					end
-end]]--
-
 function SkillActive:new(tbl)
 	tbl = tbl or {}
 	setmetatable(tbl, self)
+	Skills[tbl.id] = tbl
 	return tbl
 end
 
-function SkillActive.setupEffect()
+function SkillActive:setupEffect()
 	LOG("ERROR: SkillActive setupEffect not implemented for skill %s", self.id)
 end
 
-function SkillActive.clearEvents()
-	for _, event in pairs(SkillActive.events) do
+function SkillActive:clearEvents()
+	for _, event in pairs(self.events) do
 		event:unsubscribe()
 	end
 end
 
 function SkillActive:load()
-	LOG("LOAD ".. self.id)
-	if cplus_plus_ex.isSkillEnabled(self.id) then
-		LOG("SETTING HOOKS")
+	if cplus_plus_ex:isSkillEnabled(self.id) then
 		cplus_plus_ex:addSkillActiveHook(self.clearAndResetupEffect)
 	else
-		self.clearEvents()
+		self:clearEvents()
 	end
 end
 
 function SkillActive.clearAndResetupEffect(skillId, isActive, pawnId, pilot, skillStruct)
-	if skill.id == skillId then
+	LOG("Skill "..skillId)
+	local skillClass = Skills[skillId]
+	if skillClass then
+		LOG("Skill FOUND ACTIVE")
 		-- Clear events
-		self.clearEvents()
+		skillClass:clearEvents()
 
 		-- Then add them back if any are active
 		if cplus_plus_ex.isSkillActive(skillId) then
-			self.setupEffect()
+			skillClass:setupEffect()
 		end
 	end
 end
