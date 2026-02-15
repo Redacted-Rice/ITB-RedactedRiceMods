7c8498a48628433b13c3aa8a5517b25637bf1117
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-02-15 08:52:22 -0500

    Updating some skills and finishing up amphibious

--- RedactedRiceMods/mods/More+/scripts/trade_offs/hot_headed.lua
+++ RedactedRiceMods/mods/More+/scripts/trade_offs/hot_headed.lua
@@ -1,7 +1,7 @@
 local customSkill = more_plus.SkillActive:new{
 	id = "RrHotHeaded",
 	name = "Hot Headed",
-	description = "Gains boosted each turn but loses 2 XP per kill",
+	description = "Gains boosted every other turn but loses 2 XP per kill",
 	reusability = cplus_plus_ex.REUSABLILITY.PER_PILOT
 }
 
@@ -14,15 +14,15 @@ function customSkill.killedPawn(mission, pawn, killerId)
 	if killerId and pawn:IsEnemy() then
 		local pilot = Board:GetPawn(killerId):GetPilot()
 		if pilot and cplus_plus_ex:isSkillOnPilot(customSkill.id, pilot) then
-			-- TODO: Add effect
+			-- TODO: Add visual effect
 			pilot:addXp(-2)
 		end
 	end
 end
 
 function customSkill.boostOnTurn()
-	-- TODO: Nerf to every other turn
-	if  Game:GetTeamTurn() == TEAM_PLAYER then
+	-- Do odd turns - first turn and evey other after it
+	if  Game:GetTeamTurn() == TEAM_PLAYER and Game:GetTurnCount() % 2 == 1 then
 		for _, mechInfo in pairs(cplus_plus_ex:getMechsWithSkill(customSkill.id)) do
 			Board:GetPawn(mechInfo.pawnId):SetBoosted(true)
 		end
