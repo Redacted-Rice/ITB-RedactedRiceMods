76cbd27a387a09c6b31f052cac60ec7f88b53da9
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-03-01 20:13:59 -0500

    Fixing hot headed, hyper, and accelerator and added icons for accelerator and hyper

--- RedactedRiceMods/mods/More+/scripts/movement/accelerator.lua
+++ RedactedRiceMods/mods/More+/scripts/movement/accelerator.lua
@@ -1,30 +1,42 @@
 local MAX_MOVE = 4
+local BASE_MOVE = 1
 
 local customSkill = more_plus.SkillActive:new{
 	id = "RrAccelerator",
 	name = "Accelerator",
 	description = "+1 Move at the start of each turn (max +" .. MAX_MOVE .. ")",
 	reusability = cplus_plus_ex.REUSABLILITY.REUSABLE,
+	bonuses = {move = 1}, -- starts at one
 }
 
--- TODO:
---customSkill:addCustomTrait()
+customSkill:addCustomTrait()
 
 function customSkill:setupEffect()
 	table.insert(customSkill.events, modApi.events.onNextTurn:subscribe(customSkill.setMoveBonus))
-	self.setMoveBonus()
+	table.insert(customSkill.events, modApi.events.onMissionEnd:subscribe(customSkill.setDefaultMoveBonus))
+	table.insert(customSkill.events, modApi.events.onMissionStart:subscribe(customSkill.setDefaultMoveBonus))
+	self.setDefaultMoveBonus()
 end
 
-function customSkill.setMoveBonus()
+function customSkill:_internalSetMoveBonus(moveBonus)
 	for _, pilotAndSkills in pairs(cplus_plus_ex:getPilotsWithSkill(customSkill.id)) do
 		LOG("setMoveBonus found "..pilotAndSkills.pilot:getIdStr())
 		local pilot = pilotAndSkills.pilot
 		local idxes = pilotAndSkills.skillIndices
 		for _, idx in ipairs(idxes) do
 			local skill = pilot:getLvlUpSkill(idx)
-			skill:setMoveBonus(math.min(MAX_MOVE, Game:GetTurnCount() + 1))
+			LOG("setMoveBonus for "..pilotAndSkills.pilot:getIdStr().." at idx "..idx.. " to "..moveBonus)
+			skill:setMoveBonus(moveBonus)
 		end
 	end
+end
+
+function customSkill.setDefaultMoveBonus()
+	customSkill:_internalSetMoveBonus(BASE_MOVE)
+end
+
+function customSkill.setMoveBonus()
+	customSkill:_internalSetMoveBonus(math.min(MAX_MOVE, Game:GetTurnCount()))
 end
 
 return customSkill
