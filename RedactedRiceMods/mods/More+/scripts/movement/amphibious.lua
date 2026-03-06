47837ccbaeeb83222c16e88916a1ffe3505c406a
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-03-06 11:10:55 -0500

    Making maneuverable work with jump jets and trying to get amphibious working right with no success yet

--- RedactedRiceMods/mods/More+/scripts/movement/amphibious.lua
+++ RedactedRiceMods/mods/More+/scripts/movement/amphibious.lua
@@ -6,6 +6,22 @@ local customSkill = more_plus.SkillActive:new{
 	modified = {}
 }
 
+local function bor(a, b)
+    local result = 0
+    local bit = 1
+    while a > 0 or b > 0 do
+        local aa = a % 2
+        local bb = b % 2
+        if aa == 1 or bb == 1 then
+            result = result + bit
+        end
+        a = (a - aa) / 2
+        b = (b - bb) / 2
+        bit = bit * 2
+    end
+    return result
+end
+
 customSkill:addCustomTrait()
 
 function customSkill:setupEffect()
@@ -18,7 +34,32 @@ function customSkill.moveTargetArea(mission, pawn, weaponId, p1, targetArea)
 	if weaponId == "Move" then
 		local pilot = pawn:GetPilot()
 		if pilot and cplus_plus_ex:isSkillOnPilot(customSkill.id, pilot) and customSkill.modified[pawn:GetId()] then
-			return Board:GetReachable(point, Pawn:GetMoveSpeed(), bit32.band(Pawn:GetPathProf(), bit32.bnot(PATH_FLYER)))
+			while not targetArea:empty() do
+				targetArea:erase(0)
+			end
+			
+			local passHoles = false
+			local passThroughEnemies = "friendly"
+			if pawn:IsTeleporter() or pawn:IsJumper() or pawn:IsBurrower() then 
+				passHoles = true
+				passThroughEnemies = "none"
+			end
+			
+			-- makeAllTerrainMatcher (.., "any") == pass through any pawns
+			-- makeAllTerrainMatcher (.., "friendly") == pass through friendly pawns
+			more_plus.libs.boardUtils.getReachableInRange(targetArea, pawn:GetBaseMove(), p1,
+					-- Potentially pass over holes and enemies if jumping like movement
+					more_plus.libs.boardUtils.makeTerrainBasedMatcher(pawn, passThroughEnemies),
+					-- Can't land on holes, unpassable, cant land on any pawn
+					more_plus.libs.boardUtils.makeTerrainBasedMatcher(pawn, "any", function(point)
+								if not passholes and Board:GetTerrain(point) == TERRAIN_HOLE then
+									return true
+								end
+								if 
+								local terrain = Board:GetTerrain(point)
+								return (not passHoles and Board:GetTerrain(point) == TERRAIN_HOLE) or
+										terrain == TERRAIN_BUILDING or terrain == TERRAIN_MOUNTAIN
+							end)
 		end
 	end
 end
