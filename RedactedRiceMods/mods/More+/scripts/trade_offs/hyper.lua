04104e0b22885b5be9fe2188a384612092ad526c
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-03-09 14:46:23 -0400

    Adding visual effects for hyper and accelerator

--- RedactedRiceMods/mods/More+/scripts/trade_offs/hyper.lua
+++ RedactedRiceMods/mods/More+/scripts/trade_offs/hyper.lua
@@ -1,4 +1,5 @@
 local BASE_MOVE = 3
+local MOVE_REDUCE_COLOR = GL_Color(200, 200, 50)
 
 local customSkill = more_plus.SkillActive:new{
 	id = "RrHyper",
@@ -19,14 +20,19 @@ function customSkill:setupEffect()
 end
 
 function customSkill:_internalSetMoveBonus(moveBonus)
-	for _, pilotAndSkills in pairs(cplus_plus_ex:getPilotsWithSkill(customSkill.id)) do
-		LOG("setMoveBonus found "..pilotAndSkills.pilot:getIdStr())
-		local pilot = pilotAndSkills.pilot
-		local idxes = pilotAndSkills.skillIndices
+	for _, skillInfo in pairs(cplus_plus_ex:getMechsWithSkill(customSkill.id)) do
+		LOG("setMoveBonus found "..skillInfo.pilot:getIdStr())
+		local pilot = skillInfo.pilot
+		local idxes = skillInfo.skillIndices
 		for _, idx in ipairs(idxes) do
 			local skill = pilot:getLvlUpSkill(idx)
-			LOG("setMoveBonus for "..pilotAndSkills.pilot:getIdStr().." at idx "..idx.. " to "..moveBonus)
+			--LOG("setMoveBonus for "..skillInfo.pilot:getIdStr().." at idx "..idx.. " to "..moveBonus)
 			skill:setMoveBonus(moveBonus)
+			if Board then
+				local pawn = Board:GetPawn(skillInfo.pawnId)
+				Board:AddAlert(pawn:GetSpace(), "HYPER")
+				Board:Ping(pawn:GetSpace(), MOVE_REDUCE_COLOR)
+			end
 		end
 	end
 end
@@ -36,9 +42,12 @@ function customSkill.setDefaultMoveBonus()
 end
 
 function customSkill.setMoveBonus()
-	-- Ensure turn count is always at least 1 to avoid deployment oddities
-	local turnCount = math.max(Game:GetTurnCount(), 1)
-	customSkill:_internalSetMoveBonus(math.max(0, BASE_MOVE - turnCount + 1))
+	--LOG("TURN "..Game:GetTeamTurn())
+	if Game:GetTeamTurn() == TEAM_ENEMY then
+		-- Ensure turn count is always at least 1 to avoid deployment oddities
+		local turnCount = math.max(Game:GetTurnCount(), 1)
+		customSkill:_internalSetMoveBonus(math.max(0, BASE_MOVE - turnCount + 1))
+	end
 end
 
 return customSkill
