aeabe15f55e21a4995db0e4438f59b03b48fd82a
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-03-10 13:57:16 -0400

    Updating for new cplus+ fn

--- RedactedRiceMods/mods/More+/scripts/offensive/trophy_hunter.lua
+++ RedactedRiceMods/mods/More+/scripts/offensive/trophy_hunter.lua
@@ -24,23 +24,20 @@ end
 function customSkill.modifySkillEffect(pawn, effects)
 	local pilot = pawn:GetPilot()
 	if pilot and not effects:empty() and cplus_plus_ex:isSkillOnPilot(customSkill.id, pilot) then
-		local indexes = cplus_plus_ex:getPilotEarnedSkillIndexes(pilot)
+		local indexes = cplus_plus_ex:getPilotSkillIndices(customSkill.id, pilot)
 		for _, idx in ipairs(indexes) do
-			if pilot:getLvlUpSkill(idx):getIdStr() == customSkill.id then
-				-- Go through each space being attacked
-				for _, spaceDamage in pairs(extract_table(effects)) do
-					local spacePawn = Board:GetPawn(spaceDamage.loc)
-					if spacePawn and more_plus.libs.pawnTypeUtils.isSpawnCategory(spacePawn, "Unique") and
-							spaceDamage.iDamage > 0 and spaceDamage.iDamage ~= DAMAGE_DEATH and
-							spaceDamage.iDamage ~= DAMAGE_ZERO then
-						LOG("ADD")
-						more_plus.libs.weaponPreview.ExecuteWithState(more_plus.libs.weaponPreview.STATE_SKILL_EFFECT,
-								function() 
-									more_plus.libs.weaponPreview:AddAnimation(spaceDamage.loc, "rr_hunter_"..idx)
-								end)
-						lastLoc = spaceDamage.loc
-						spaceDamage.iDamage = spaceDamage.iDamage + 1
-					end
+			for _, spaceDamage in pairs(extract_table(effects)) do
+				local spacePawn = Board:GetPawn(spaceDamage.loc)
+				if spacePawn and more_plus.libs.pawnTypeUtils.isSpawnCategory(spacePawn, "Unique") and
+						spaceDamage.iDamage > 0 and spaceDamage.iDamage ~= DAMAGE_DEATH and
+						spaceDamage.iDamage ~= DAMAGE_ZERO then
+					LOG("ADD")
+					more_plus.libs.weaponPreview.ExecuteWithState(more_plus.libs.weaponPreview.STATE_SKILL_EFFECT,
+							function() 
+								more_plus.libs.weaponPreview:AddAnimation(spaceDamage.loc, "rr_hunter_"..idx)
+							end)
+					lastLoc = spaceDamage.loc
+					spaceDamage.iDamage = spaceDamage.iDamage + 1
 				end
 			end
 		end
