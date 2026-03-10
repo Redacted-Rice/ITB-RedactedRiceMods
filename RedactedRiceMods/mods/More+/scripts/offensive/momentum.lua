cbd14b8908d2c2bbe61089f30d9d111a1e292b00
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-03-10 11:48:27 -0400

    Fixing tracking and undoing of move based abiilities

--- RedactedRiceMods/mods/More+/scripts/offensive/momentum.lua
+++ RedactedRiceMods/mods/More+/scripts/offensive/momentum.lua
@@ -5,12 +5,14 @@ local customSkill = more_plus.SkillActive:new{
 	name = "Momentum",
 	description = "Gains boosted after moving at least 4 tiles",
 	reusability = cplus_plus_ex.REUSABLILITY.PER_PILOT,
+	notPreBoosted = {},
 }
 
 customSkill:addCustomTrait()
 
 function customSkill:setupEffect()
 	table.insert(customSkill.events, modapiext.events.onSkillBuild:subscribe(customSkill.checkMove))
+	table.insert(customSkill.events, modapiext.events.onPawnUndoMove:subscribe(customSkill.undoBoosted))
 end
 
 function customSkill.checkMove(mission, pawn, weaponId, p1, p2, skillEffect)
@@ -19,15 +21,30 @@ function customSkill.checkMove(mission, pawn, weaponId, p1, p2, skillEffect)
 		if pilot and cplus_plus_ex:isSkillOnPilot(customSkill.id, pilot) then
 			local distance = math.abs(p2.x - p1.x) + math.abs(p2.y - p1.y)
 			LOGF("Momentum: Pawn %d moving %d tiles (from %s to %s)", pawn:GetId(), distance, p1:GetString(), p2:GetString())
+
 			if distance >= MIN_DISTANCE then
-				local pawnId = pawn:GetId()
-				local boostDamage = SpaceDamage(p2, 0)
-				boostDamage.sScript = "local pawn = Board:GetPawn("..pawnId..") pawn:SetBoosted(true)"
-				skillEffect:AddDamage(boostDamage)
-				LOGF("Momentum: Will apply boosted to pawn %d (moving %d tiles)", pawnId, distance)
+				if not pawn:IsBoosted() then
+					local pawnId = pawn:GetId()
+					local boostDamage = SpaceDamage(p2, 0)
+					boostDamage.sScript = [[
+						Board:GetPawn(]]..pawnId..[[):SetBoosted(true)
+						more_plus.SkillActive.skills.RrMomentum.notPreBoosted[]]..pawnId..[[] = true
+					]]
+					skillEffect:AddDamage(boostDamage)
+					LOGF("Momentum: Will apply boosted to pawn %d (moving %d tiles)", pawnId, distance)
+				end
 			end
 		end
 	end
 end
 
+function customSkill.undoBoosted(mission, pawn, undonePosition)
+	if customSkill.notPreBoosted[pawn:GetId()] then
+		pawn:SetBoosted(false)
+		customSkill.notPreBoosted[pawn:GetId()] = nil
+		LOG("Momentum: Removed boosted from pawn " .. pawn:GetId())
+	end
+end
+
 return customSkill
+
--- RedactedRiceMods/mods/More+/scripts/trade_offs/kickoff_boosters.lua
+++ RedactedRiceMods/mods/More+/scripts/trade_offs/kickoff_boosters.lua
@@ -3,7 +3,7 @@ local customSkill = more_plus.SkillActive:new{
 	name = "Kickoff Boosters",
 	description = "When moving, cracks tile moved from",
 	reusability = cplus_plus_ex.REUSABLILITY.PER_PILOT,
-	notPreCracked = {},
+	moveStartPositions = {},
 }
 
 customSkill:addCustomTrait()
@@ -16,21 +16,25 @@ end
 function customSkill.moveSkillBuild(mission, pawn, weaponId, p1, p2, skillEffect)
 	if weaponId == "Move" then
 		local pilot = pawn:GetPilot()
-		if not Board:IsCracked(p1) and pilot and cplus_plus_ex:isSkillOnPilot(customSkill.id, pilot) then
-			LOG("CRACKING SPACE "..p1:GetString())
-			customSkill.notPreCracked[pawn:GetId()] = true
-			local damageC = SpaceDamage(p1, 0)
-			damageC.iCrack = EFFECT_CREATE
-			skillEffect:AddDamage(damageC)
-		else
-			customSkill.notPreCracked[pawn:GetId()] = nil
+		if pilot and cplus_plus_ex:isSkillOnPilot(customSkill.id, pilot) then
+			if not Board:IsCracked(p1) then
+				local damageC = SpaceDamage(p1, 0)
+				damageC.iCrack = EFFECT_CREATE
+				boostDamage.sScript = [[
+					more_plus.SkillActive.skills.RrKickoffBoosters.moveStartPositions[]]..pawnId..[[] = ]] .. p1:GetString()
+				skillEffect:AddDamage(damageC)
+				LOGF("Kickoff Boosters: Will crack %s when pawn %d moves", p1:GetString(), pawn:GetId())
+			end
 		end
 	end
 end
 
 function customSkill.undoCracked(mission, pawn, undonePosition)
-	if customSkill.notPreCracked[pawn:GetId()] then
-		Board:SetCracked(pawn:GetSpace(), false)
+	local startPos = customSkill.moveStartPositions[pawn:GetId()]
+	if startPos then
+		Board:SetCracked(startPos, false)
+		customSkill.moveStartPositions[pawn:GetId()] = nil
+		LOGF("Kickoff Boosters: Uncracked %s for pawn %d (move undone)", startPos:GetString(), pawn:GetId())
 	end
 end
 
