2d19d52dd4acacbe66ce81c484b2a4160e43daa1
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-03-10 11:16:43 -0400

    First cut at momentum and first blood

---
+++ RedactedRiceMods/mods/More+/scripts/offensive/momentum.lua
@@ -0,0 +1,33 @@
+local MIN_DISTANCE = 4
+
+local customSkill = more_plus.SkillActive:new{
+	id = "RrMomentum",
+	name = "Momentum",
+	description = "Gains boosted after moving at least 4 tiles",
+	reusability = cplus_plus_ex.REUSABLILITY.PER_PILOT,
+}
+
+customSkill:addCustomTrait()
+
+function customSkill:setupEffect()
+	table.insert(customSkill.events, modapiext.events.onSkillBuild:subscribe(customSkill.checkMove))
+end
+
+function customSkill.checkMove(mission, pawn, weaponId, p1, p2, skillEffect)
+	if weaponId == "Move" then
+		local pilot = pawn:GetPilot()
+		if pilot and cplus_plus_ex:isSkillOnPilot(customSkill.id, pilot) then
+			local distance = math.abs(p2.x - p1.x) + math.abs(p2.y - p1.y)
+			LOGF("Momentum: Pawn %d moving %d tiles (from %s to %s)", pawn:GetId(), distance, p1:GetString(), p2:GetString())
+			if distance >= MIN_DISTANCE then
+				local pawnId = pawn:GetId()
+				local boostDamage = SpaceDamage(p2, 0)
+				boostDamage.sScript = "local pawn = Board:GetPawn("..pawnId..") pawn:SetBoosted(true)"
+				skillEffect:AddDamage(boostDamage)
+				LOGF("Momentum: Will apply boosted to pawn %d (moving %d tiles)", pawnId, distance)
+			end
+		end
+	end
+end
+
+return customSkill
