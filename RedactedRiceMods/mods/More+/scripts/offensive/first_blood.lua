5e1205768352dc37ddd21da4f6640b80361bdba2
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-03-20 11:10:14 -0400

    First blood fix

--- RedactedRiceMods/mods/More+/scripts/offensive/first_blood.lua
+++ RedactedRiceMods/mods/More+/scripts/offensive/first_blood.lua
@@ -7,6 +7,9 @@ local customSkill = more_plus.SkillActive:new{
 
 customSkill:addCustomTrait()
 
+-- Track processed spaces to prevent double triggering on skill build and final effect build
+customSkill.processedDamages = {}
+
 function customSkill:setupEffect()
 	table.insert(customSkill.events, modapiext.events.onSkillBuild:subscribe(
 		function(mission, pawn, weaponId, p1, p2, skillEffect)
@@ -20,6 +23,10 @@ function customSkill:setupEffect()
 		end))
 end
 
+function hashSpaceDamage(point, index)
+	return "p_"..point.x.."_"..point.y
+end
+
 function customSkill.modifySkillEffect(pawn, effects)
 	if not pawn then
 		return
@@ -27,23 +34,36 @@ function customSkill.modifySkillEffect(pawn, effects)
 	local pilot = pawn:GetPilot()
 	if pilot and not effects:empty() and cplus_plus_ex:isSkillOnPilot(customSkill.id, pilot) then
 		local indexes = cplus_plus_ex:getPilotSkillIndices(customSkill.id, pilot)
-		for _, idx in ipairs(indexes) do
-			for _, spaceDamage in pairs(extract_table(effects)) do
+
+		for i, spaceDamage in pairs(extract_table(effects)) do
+			local spaceDamageKey = hashSpaceDamage(spaceDamage.loc, i)
+			LOG(spaceDamageKey)
+			if not customSkill.processedDamages[spaceDamageKey] then
+				customSkill.processedDamages[spaceDamageKey] = true
 				local spacePawn = Board:GetPawn(spaceDamage.loc)
+
 				if spacePawn and spacePawn:IsEnemy() and
 						spacePawn:GetHealth() == _G[spacePawn:GetType()].Health and
 						spacePawn:GetHealth() >= 4 and spaceDamage.iDamage > 0 and
 						spaceDamage.iDamage ~= DAMAGE_DEATH and spaceDamage.iDamage ~= DAMAGE_ZERO then
+					
+					modApi:runLater(function()
+						more_plus.SkillActive.skills.RrFirstBlood.processedDamages = {}
+					end)
+					
+					for _, idx in ipairs(indexes) do
+						more_plus.libs.weaponPreview.ExecuteWithState(more_plus.libs.weaponPreview.STATE_SKILL_EFFECT,
+								function()
+									more_plus.libs.weaponPreview:AddAnimation(spaceDamage.loc,
+											more_plus.commonIcons.extraDamage.key.."_"..idx)
+								end)
 
-					more_plus.libs.weaponPreview.ExecuteWithState(more_plus.libs.weaponPreview.STATE_SKILL_EFFECT,
-							function()
-								more_plus.libs.weaponPreview:AddAnimation(spaceDamage.loc,
-										more_plus.commonIcons.extraDamage.key.."_"..idx)
-							end)
-
-					spaceDamage.iDamage = spaceDamage.iDamage + 1
-					LOG("First Blood: Added +1 damage to undamaged vek with 4+ health at ".. spaceDamage.loc:GetString())
+						spaceDamage.iDamage = spaceDamage.iDamage + 1
+						LOG("First Blood: Added +1 damage to undamaged vek with 4+ health at ".. spaceDamage.loc:GetString())
+					end
 				end
+			else
+				LOG("First Blood: Already processed damage at ".. spaceDamage.loc:GetString())
 			end
 		end
 	end
