5dbf04b520bd59271b357138e1649a3922deff02
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-03-22 19:39:33 -0400

    Adding images for streetwise

--- RedactedRiceMods/mods/More+/scripts/more_plus.lua
+++ RedactedRiceMods/mods/More+/scripts/more_plus.lua
@@ -84,6 +84,7 @@ more_plus.commonIcons = {
 	extraDamage = {key = "rr_extra_damage", img =  "combat/icons/icon_mp_RrExtraDamage.png", pos1 = Point(-25,11), pos2 = Point(-18,-4)},
 	crit = {key = "rr_crit", img =  "combat/icons/icon_mp_RrCrit.png", pos1 = Point(-25,11), pos2 = Point(-18,-4)},
 	shackle = {key = "rr_shackle", img =  "combat/icons/icon_mp_RrShackle.png", pos1 = Point(-25,11), pos2 = Point(-18,-4)},
+	noDamage = {key = "rr_no_damage", img =  "combat/icons/icon_mp_RrNoDamage.png", pos1 = Point(-25,11), pos2 = Point(-18,-4)},
 }
 
 function more_plus:addCommonCustomImages()
