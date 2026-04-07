dd8c72c8e478a0d9a6430977aade1fc52206cf0d
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-04-07 13:21:12 -0400

    Adding positioning skills - ambusher, escort and rally

---
+++ RedactedRiceMods/mods/More+/scripts/positioning/ambusher.lua
@@ -0,0 +1,51 @@
+local customSkill = more_plus.SkillEffectModifier:new{
+	id = "RrAmbusher",
+	name = "Ambusher",
+	description = "Gain +1 damage if not on road, liquid, or hole.",
+	reusability = cplus_plus_ex.REUSABLILITY.PER_PILOT,
+}
+
+customSkill.DEBUG = false
+local logger = memhack.logger
+local SUBMODULE = logger.register("More+", "Ambusher", customSkill.DEBUG)
+
+customSkill:addCustomTrait()
+
+function customSkill:modifySpaceDamage(pawn, isFinalEffect, spaceDamage, indexes, spacePawn)
+	-- Check if this is damage from the pawn with ambusher skill
+	if spaceDamage.iDamage > 0 and spaceDamage.iDamage ~= DAMAGE_DEATH and spaceDamage.iDamage ~= DAMAGE_ZERO then
+		local pawnLoc = pawn:GetSpace()
+		local terrain = Board:GetTerrain(pawnLoc)
+
+		-- Check if NOT on road, liquid, or hole
+		local isOnRoad = (terrain == TERRAIN_ROAD)
+		local isOnLiquid = (terrain == TERRAIN_WATER or terrain == TERRAIN_LAVA or terrain == TERRAIN_ACID)
+		local isOnHole = (terrain == TERRAIN_HOLE)
+
+		if not isOnRoad and not isOnLiquid and not isOnHole then
+			local previewState = isFinalEffect and more_plus.libs.weaponPreview.STATE_FINAL_EFFECT or
+					more_plus.libs.weaponPreview.STATE_SKILL_EFFECT
+
+			for _, idx in ipairs(indexes) do
+				logger.logDebug(SUBMODULE, "Adding ambush damage icon for %s with idx %d",
+						spaceDamage.loc:GetString(), idx)
+				more_plus.libs.weaponPreview.ExecuteWithState(previewState,
+						function()
+							more_plus.libs.weaponPreview:AddAnimation(spaceDamage.loc,
+									more_plus.commonIcons.extraDamage.key.."_"..idx)
+						end)
+			end
+
+			spaceDamage.iDamage = spaceDamage.iDamage + 1
+			logger.logDebug(SUBMODULE, "Added +1 ambush damage at %s (terrain: %d)",
+					spaceDamage.loc:GetString(), terrain)
+		else
+			logger.logDebug(SUBMODULE, "No ambush bonus - on road/liquid/hole (terrain: %d, hole: %s)",
+					terrain, tostring(isOnHole))
+		end
+	end
+
+	return nil
+end
+
+return customSkill
---
+++ RedactedRiceMods/mods/More+/scripts/positioning/rally.lua
@@ -0,0 +1,97 @@
+local customSkill = more_plus.SkillActive:new{
+	id = "RrRally",
+	name = "Rally",
+	description = "Boost adjacent allies when you move next to them or they move next to you.",
+	reusability = cplus_plus_ex.REUSABLILITY.PER_PILOT,
+	boostedPawns = {},
+}
+
+customSkill.DEBUG = false
+local logger = memhack.logger
+local SUBMODULE = logger.register("More+", "Rally", customSkill.DEBUG)
+
+-- Exclude pilots who already give boosted
+cplus_plus_ex:registerPilotSkillExclusions("Pilot_Arrogant", customSkill.id)
+cplus_plus_ex:registerPilotSkillExclusions("Pilot_Chemical", customSkill.id)
+
+customSkill:addCustomTrait()
+
+function customSkill:setupEffect()
+	table.insert(customSkill.events, modapiext.events.onSkillBuild:subscribe(customSkill.moveSkillBuild))
+	table.insert(customSkill.events, modapiext.events.onPawnUndoMove:subscribe(customSkill.undoBoosted))
+end
+
+function customSkill.moveSkillBuild(mission, pawn, weaponId, p1, p2, skillEffect)
+	if weaponId == "Move" then
+		-- Check if the moving pawn has Rally skill
+		local movingPilot = pawn:GetPilot()
+		if movingPilot and cplus_plus_ex:isSkillOnPilot(customSkill.id, movingPilot) then
+			-- Moving pawn with Rally - check for adjacent allies at destination
+			for dir = DIR_START, DIR_END do
+				local adjacentLoc = p2 + DIR_VECTORS[dir]
+				if Board:IsValid(adjacentLoc) then
+					local adjacentPawn = Board:GetPawn(adjacentLoc)
+					if adjacentPawn and adjacentPawn:IsMech() and not adjacentPawn:IsBoosted() then
+						logger.logDebug(SUBMODULE, "Rally pawn %d moving to %s, boosting adjacent ally %d at %s",
+							pawn:GetId(), p2:GetString(), adjacentPawn:GetId(), adjacentLoc:GetString())
+
+						more_plus.libs.weaponPreview.ExecuteWithState(more_plus.libs.weaponPreview.STATE_SKILL_EFFECT,
+								function()
+									more_plus.libs.weaponPreview:AddAnimation(adjacentLoc,
+											more_plus.commonIcons.boost.key.."_1")
+								end)
+
+						local boostDamage = SpaceDamage(adjacentLoc, 0)
+						boostDamage.sScript = string.format([[
+								more_plus.SkillActive.skills.RrRally.boostedPawns[%d] = true
+								Board:GetPawn(%d):SetBoosted(true)
+								]], adjacentPawn:GetId(), adjacentPawn:GetId())
+						skillEffect:AddDamage(boostDamage)
+					end
+				end
+			end
+		end
+
+		-- Check if moving pawn is moving adjacent to a pawn with Rally
+		for dir = DIR_START, DIR_END do
+			local adjacentLoc = p2 + DIR_VECTORS[dir]
+			if Board:IsValid(adjacentLoc) then
+				local adjacentPawn = Board:GetPawn(adjacentLoc)
+				if adjacentPawn and adjacentPawn:IsMech() then
+					local adjacentPilot = adjacentPawn:GetPilot()
+					if adjacentPilot and cplus_plus_ex:isSkillOnPilot(customSkill.id, adjacentPilot) then
+						-- Found Rally pilot, boost the moving pawn if not already boosted
+						if not pawn:IsBoosted() then
+							logger.logDebug(SUBMODULE, "Pawn %d moving adjacent to Rally pawn %d, boosting",
+								pawn:GetId(), adjacentPawn:GetId())
+
+							more_plus.libs.weaponPreview.ExecuteWithState(more_plus.libs.weaponPreview.STATE_SKILL_EFFECT,
+									function()
+										more_plus.libs.weaponPreview:AddAnimation(p2,
+												more_plus.commonIcons.boost.key.."_1")
+									end)
+
+							local boostDamage = SpaceDamage(p2, 0)
+							boostDamage.sScript = string.format([[
+									more_plus.SkillActive.skills.RrRally.boostedPawns[%d] = true
+									Board:GetPawn(%d):SetBoosted(true)
+									]], pawn:GetId(), pawn:GetId())
+							skillEffect:AddDamage(boostDamage)
+							break
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+function customSkill.undoBoosted(mission, pawn, undonePosition)
+	if customSkill.boostedPawns[pawn:GetId()] then
+		pawn:SetBoosted(false)
+		customSkill.boostedPawns[pawn:GetId()] = nil
+		logger.logDebug(SUBMODULE, "Removed boosted from pawn %d (move undone)", pawn:GetId())
+	end
+end
+
+return customSkill
