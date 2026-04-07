9ee6e65c6345c22773b9d44fc89939e5dce18a8c
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-04-07 15:01:39 -0400

    Adding adjacent fn to board utils and updating position skills to use those where appropriate

--- RedactedRiceMods/mods/More+/scripts/positioning/rally.lua
+++ RedactedRiceMods/mods/More+/scripts/positioning/rally.lua
@@ -27,61 +27,58 @@ function customSkill.moveSkillBuild(mission, pawn, weaponId, p1, p2, skillEffect
 		local movingPilot = pawn:GetPilot()
 		if movingPilot and cplus_plus_ex:isSkillOnPilot(customSkill.id, movingPilot) then
 			-- Moving pawn with Rally - check for adjacent allies at destination
-			for dir = DIR_START, DIR_END do
-				local adjacentLoc = p2 + DIR_VECTORS[dir]
-				if Board:IsValid(adjacentLoc) then
+			local adjacentMechs = more_plus.libs.boardUtils.getAdjacent(p2, function(adjacentLoc)
 					local adjacentPawn = Board:GetPawn(adjacentLoc)
-					if adjacentPawn and adjacentPawn:IsMech() and not adjacentPawn:IsBoosted() then
-						logger.logDebug(SUBMODULE, "Rally pawn %d moving to %s, boosting adjacent ally %d at %s",
-							pawn:GetId(), p2:GetString(), adjacentPawn:GetId(), adjacentLoc:GetString())
+					return adjacentPawn and adjacentPawn:IsMech() and not adjacentPawn:IsBoosted()
+			end)
 
-						more_plus.libs.weaponPreview.ExecuteWithState(more_plus.libs.weaponPreview.STATE_SKILL_EFFECT,
-								function()
-									more_plus.libs.weaponPreview:AddAnimation(adjacentLoc,
-											more_plus.commonIcons.boost.key.."_1")
-								end)
+			for _, adjacentLoc in ipairs(adjacentMechs) do
+				local adjacentPawn = Board:GetPawn(adjacentLoc)
+				logger.logDebug(SUBMODULE, "Rally pawn %d moving to %s, boosting adjacent ally %d at %s",
+					pawn:GetId(), p2:GetString(), adjacentPawn:GetId(), adjacentLoc:GetString())
 
-						local boostDamage = SpaceDamage(adjacentLoc, 0)
-						boostDamage.sScript = string.format([[
-								more_plus.SkillActive.skills.RrRally.boostedPawns[%d] = true
-								Board:GetPawn(%d):SetBoosted(true)]],
-								adjacentPawn:GetId(), adjacentPawn:GetId())
-						skillEffect:AddDamage(boostDamage)
-					end
-				end
+				more_plus.libs.weaponPreview.ExecuteWithState(more_plus.libs.weaponPreview.STATE_SKILL_EFFECT,
+						function()
+							more_plus.libs.weaponPreview:AddAnimation(adjacentLoc,
+									more_plus.commonIcons.boost.key.."_1")
+						end)
+
+				local boostDamage = SpaceDamage(adjacentLoc, 0)
+				boostDamage.sScript = string.format([[
+						more_plus.SkillActive.skills.RrRally.boostedPawns[%d] = true
+						Board:GetPawn(%d):SetBoosted(true)]],
+						adjacentPawn:GetId(), adjacentPawn:GetId())
+				skillEffect:AddDamage(boostDamage)
 			end
 		end
 
 		-- Check if moving pawn is moving adjacent to a pawn with Rally
-		for dir = DIR_START, DIR_END do
-			local adjacentLoc = p2 + DIR_VECTORS[dir]
-			if Board:IsValid(adjacentLoc) then
-				local adjacentPawn = Board:GetPawn(adjacentLoc)
-				if adjacentPawn and adjacentPawn:IsMech() then
-					local adjacentPilot = adjacentPawn:GetPilot()
-					if adjacentPilot and cplus_plus_ex:isSkillOnPilot(customSkill.id, adjacentPilot) then
-						-- Found Rally pilot, boost the moving pawn if not already boosted
-						if not pawn:IsBoosted() then
-							logger.logDebug(SUBMODULE, "Pawn %d moving adjacent to Rally pawn %d, boosting",
-								pawn:GetId(), adjacentPawn:GetId())
-
-							more_plus.libs.weaponPreview.ExecuteWithState(more_plus.libs.weaponPreview.STATE_SKILL_EFFECT,
-									function()
-										more_plus.libs.weaponPreview:AddAnimation(p2,
-												more_plus.commonIcons.boost.key.."_1")
-									end)
-
-							local boostDamage = SpaceDamage(p2, 0)
-							boostDamage.sScript = string.format([[
-									more_plus.SkillActive.skills.RrRally.boostedPawns[%d] = true
-									Board:GetPawn(%d):SetBoosted(true)]],
-									pawn:GetId(), pawn:GetId())
-							skillEffect:AddDamage(boostDamage)
-							break
-						end
-					end
-				end
+		local hasAdjacentRally = more_plus.libs.boardUtils.isAdjacent(p2, function(adjacentLoc)
+			local adjacentPawn = Board:GetPawn(adjacentLoc)
+			if adjacentPawn and adjacentPawn:IsMech() then
+				local adjacentPilot = adjacentPawn:GetPilot()
+				return adjacentPilot and cplus_plus_ex:isSkillOnPilot(customSkill.id, adjacentPilot)
 			end
+			return false
+		end)
+		
+		-- Boost the moving pawn if not already boosted and found a Rally pilot
+		if hasAdjacentRally and not pawn:IsBoosted() then
+			logger.logDebug(SUBMODULE, "Pawn %d moving adjacent to Rally pawn, boosting",
+				pawn:GetId())
+
+			more_plus.libs.weaponPreview.ExecuteWithState(more_plus.libs.weaponPreview.STATE_SKILL_EFFECT,
+					function()
+						more_plus.libs.weaponPreview:AddAnimation(p2,
+								more_plus.commonIcons.boost.key.."_1")
+					end)
+
+			local boostDamage = SpaceDamage(p2, 0)
+			boostDamage.sScript = string.format([[
+					more_plus.SkillActive.skills.RrRally.boostedPawns[%d] = true
+					Board:GetPawn(%d):SetBoosted(true)]],
+					pawn:GetId(), pawn:GetId())
+			skillEffect:AddDamage(boostDamage)
 		end
 	end
 end
