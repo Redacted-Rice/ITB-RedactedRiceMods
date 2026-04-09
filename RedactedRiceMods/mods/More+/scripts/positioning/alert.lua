f4bef13753d53ee79da2329c60a741acf41926e3
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-04-09 09:44:40 -0400

    Fixing alert to use damage reduction instead of armor

--- RedactedRiceMods/mods/More+/scripts/positioning/alert.lua
+++ RedactedRiceMods/mods/More+/scripts/positioning/alert.lua
@@ -1,7 +1,7 @@
-local customSkill = more_plus.SkillActive:new{
+local customSkill = more_plus.SkillEffectModifier:new{
 	id = "RrAlert",
 	name = "Alert",
-	description = "Gain armor while adjacent to a vek.",
+	description = "Reduce damage taken by 1 while adjacent to a vek.",
 	reusability = cplus_plus_ex.REUSABLILITY.PER_PILOT,
 }
 
@@ -9,24 +9,14 @@ customSkill.DEBUG = false
 local logger = memhack.logger
 local SUBMODULE = logger.register("More+", "Alert", customSkill.DEBUG)
 
--- Excluse Abe with his innate armor
-cplus_plus_ex:registerPilotSkillExclusions("Pilot_Assassin", customSkill.id)
-
 customSkill:addCustomTrait()
 
 function customSkill:setupEffect()
-	-- Track move skill builds to show icons when moving adjacent to vek. Actually armor will be
-	-- applied once moved via other callbacks
+	-- Call parent's setupEffect to register with SkillEffectModifier system
+	more_plus.SkillEffectModifier.setupEffect(self)
+
+	-- Track move skill builds to show icons when moving adjacent to vek
 	table.insert(customSkill.events, modapiext.events.onSkillBuild:subscribe(customSkill.moveSkillBuild))
-
-	-- Any time a new pawn comes or a pawn changes positions, just recalculate all
-	-- pawns with the skill
-	table.insert(customSkill.events, modapiext.events.onPawnPositionChanged:subscribe(customSkill.pawnsChanged))
-	table.insert(customSkill.events, modapiext.events.onPawnTracked:subscribe(customSkill.pawnsChanged))
-	table.insert(customSkill.events, modapiext.events.onPawnUntracked:subscribe(customSkill.pawnsChanged))
-
-	-- Track mission end to remove any remaining temp armor
-	table.insert(customSkill.events, modapiext.events.onMissionEnd:subscribe(customSkill.onMissionEnd))
 end
 
 -- Check if a location is adjacent to any vek
@@ -37,27 +27,38 @@ function customSkill.isAdjacentToVek(loc)
 	end)
 end
 
--- Apply or remove armor based on adjacency to vek
-function customSkill.updateArmorForPawn(pawn)
-	-- only pawns with skill will have this called so we don't need to check again
+-- SkillEffectModifier implementation: reduce damage by 1 if target is adjacent to vek
+function customSkill:modifySpaceDamage(pawn, isFinalEffect, spaceDamage, indexes, spacePawn)
+	-- Check if the target pawn is taking damage and is adjacent to a vek
+	if spacePawn and spaceDamage.iDamage > 0 and spaceDamage.iDamage ~= DAMAGE_ZERO and spaceDamage.iDamage ~= DAMAGE_DEATH then
+		local targetLoc = spaceDamage.loc
 
-	-- Check if the pawn innately has armor (via _G[pawn:GetType()]) and if so, skip
-	local pawnType = _G[pawn:GetType()]
-	if pawnType and pawnType.Armor then
-		logger.logDebug(SUBMODULE, "Pawn %d (%s) has innate armor (%d), skipping Alert skill",
-				pawn:GetId(), pawn:GetType(), pawnType.Armor)
-		return
+		if customSkill.isAdjacentToVek(targetLoc) then
+			-- Reduce damage by 1
+			spaceDamage.iDamage = math.max(0, spaceDamage.iDamage - 1)
+
+			-- TODO: Need to make a different armor icon since this won't behave as vanilla
+
+			-- Show damage reduction icon
+			local previewState = isFinalEffect and more_plus.libs.weaponPreview.STATE_FINAL_EFFECT or
+					more_plus.libs.weaponPreview.STATE_SKILL_EFFECT
+
+			for _, idx in ipairs(indexes) do
+				logger.logDebug(SUBMODULE, "Adding damage reduction icon for %s with idx %d",
+						spaceDamage.loc:GetString(), idx)
+				more_plus.libs.weaponPreview.ExecuteWithState(previewState,
+						function()
+							more_plus.libs.weaponPreview:AddAnimation(spaceDamage.loc,
+									more_plus.commonIcons.armor.key.."_"..idx)
+						end)
+			end
+
+			logger.logDebug(SUBMODULE, "Alert reduced damage for pawn %d at %s (adjacent to vek)",
+					spacePawn:GetId(), targetLoc:GetString())
+		end
 	end
 
-	local pawnLoc = pawn:GetSpace()
-	local shouldHaveArmor = customSkill.isAdjacentToVek(pawnLoc)
-	-- TODO: Not implemented yet. Need to investigate and add via memhack
-	pawn:SetArmor(shouldHaveArmor)
-	if shouldHaveArmor then
-		logger.logDebug(SUBMODULE, "Added armor to pawn %d at %s", pawn:GetId(), pawnLoc:GetString())
-	else
-		logger.logDebug(SUBMODULE, "Removed armor from pawn %d at %s", pawn:GetId(), pawnLoc:GetString())
-	end
+	return nil
 end
 
 -- Show icon when moving to a location adjacent to vek
@@ -67,50 +68,16 @@ function customSkill.moveSkillBuild(mission, pawn, weaponId, p1, p2, skillEffect
 		if pilot and cplus_plus_ex:isSkillOnPilot(customSkill.id, pilot) then
 			local willBeAdjacentToVek = customSkill.isAdjacentToVek(p2)
 
-			-- Show icon if gaining armor
+			-- Show icon if will be adjacent to vek
 			if willBeAdjacentToVek then
 				more_plus.libs.weaponPreview.ExecuteWithState(more_plus.libs.weaponPreview.STATE_SKILL_EFFECT,
 						function()
 							more_plus.libs.weaponPreview:AddAnimation(p2,
 									more_plus.commonIcons.armor.key.."_1")
 						end)
-				logger.logDebug(SUBMODULE, "Pawn %d moving to %s adjacent to vek, showing armor icon",
+				logger.logDebug(SUBMODULE, "Pawn %d moving to %s adjacent to vek, showing damage reduction icon",
 						pawn:GetId(), p2:GetString())
 			end
-		end
-	end
-end
-
--- Handle new pawns being tracked or position changes
-function customSkill.pawnsChanged(mission, pawn)
-	-- Get all pawns with the Alert skill and update their armor
-	local pawnsWithSkill = cplus_plus_ex:getMechsWithSkill(customSkill.id, true)
-
-	logger.logDebug(SUBMODULE, "Pawns changed event - updating %d pawns with Alert skill", #pawnsWithSkill)
-
-	for _, alertPawn in ipairs(pawnsWithSkill) do
-		customSkill.updateArmorForPawn(alertPawn)
-	end
-end
-
--- Remove armor on mission end for all pawns with skill
-function customSkill.onMissionEnd(mission)
-	-- Get all pawns with the Alert skill using CPLUS+ function
-	local pawnsWithSkill = cplus_plus_ex:getMechsWithSkill(customSkill.id, true)
-
-	logger.logDebug(SUBMODULE, "Mission ending - removing armor from %d pawns with Alert skill", #pawnsWithSkill)
-
-	for _, alertPawn in ipairs(pawnsWithSkill) do
-		-- Check if the pawn innately has armor
-		local pawnType = _G[alertPawn:GetType()]
-		local hasInnateArmor = pawnType and pawnType.Armor
-
-		if not hasInnateArmor then
-			-- Remove any armor granted by the skill
-			alertPawn:SetArmor(false)
-			logger.logDebug(SUBMODULE, "Removed Alert armor from pawn %d at mission end", alertPawn:GetId())
-		else
-			logger.logDebug(SUBMODULE, "Pawn %d has innate armor, not removing", alertPawn:GetId())
 		end
 	end
 end
