182331a1e6ff557ca49662854a8c5185196a641d
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-04-17 23:22:05 -0400

    Adding vampire, reflect icons and anger. Updating on grid icons to be _glow

--- RedactedRiceMods/mods/More+/scripts/offensive/vampire.lua
+++ RedactedRiceMods/mods/More+/scripts/offensive/vampire.lua
@@ -28,14 +28,30 @@ function customSkill:modifySpaceDamage(source, attackingPawn, previewState, spac
 		end
 
 		if wouldKill then
-			local pawnLoc = attackingPawn:GetSpace()
-
+			local attackerLoc = attackingPawn:GetSpace()
+			local targetLoc = targetPawn:GetSpace()
+		
+			-- Add vampire animation icons
+			for _, idx in ipairs(indexes) do
+				-- Show damage icon on attacker (where reflect damage will hit)
+				logger.logDebug(SUBMODULE, "Adding reflect damage icon from %s to attacker %s with idx %d", 
+						targetLoc:GetString(), attackerLoc:GetString(), idx)
+				more_plus.libs.weaponPreview.ExecuteWithState(previewState,
+						function()
+							-- add to attacker and target
+							more_plus.libs.weaponPreview:AddAnimation(attackerLoc,
+									more_plus.commonIcons.vampire.key.."_"..idx)
+							more_plus.libs.weaponPreview:AddAnimation(targetLoc,
+									more_plus.commonIcons.vampire.key.."_"..idx)
+						end)
+			end
+		
 			-- Call repair skill and return array of all the space damages from it
-			local repairEffect = _G["Skill_Repair"]:GetSkillEffect(pawnLoc, pawnLoc)
+			local repairEffect = _G["Skill_Repair"]:GetSkillEffect(attackerLoc, attackerLoc)
 			local repairEffectTable = extract_table(repairEffect.effect)
 
 			logger.logDebug(SUBMODULE, "Getting repair effect for pawn %d at %s (killed vek at %s)",
-					attackingPawn:GetId(), pawnLoc:GetString(), spaceDamage.loc:GetString())
+					attackingPawn:GetId(), attackerLoc:GetString(), spaceDamage.loc:GetString())
 			return repairEffectTable
 		end
 	end
