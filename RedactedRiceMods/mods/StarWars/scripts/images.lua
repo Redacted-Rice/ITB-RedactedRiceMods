c092cfc4c716abd89fed885f7f6cc2f39fed6be6
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-04-21 08:35:42 -0400

    Got a custom image working. Needs tweaking

--- RedactedRiceMods/mods/StarWars/scripts/images.lua
+++ RedactedRiceMods/mods/StarWars/scripts/images.lua
@@ -1,7 +1,9 @@
-local resourcePath = mod_loader.mods[modApi.currentMod].resourcePath
+--local resourcePath = mod_loader.mods[modApi.currentMod].resourcePath
 
 --Weapons
 --modApi:appendAsset("img/weapons/prime_wb_mold.png",    resourcePath .. "img/weapons/prime_wb_mold.png")
 --modApi:appendAsset("img/weapons/brute_wb_consume.png", resourcePath .. "img/weapons/brute_wb_consume.png")
 --modApi:appendAsset("img/weapons/science_wb_shift.png", resourcePath .. "img/weapons/science_wb_shift.png")
 --modApi:appendAsset("img/weapons/passives/passive_wb_move.png",  resourcePath .. "img/weapons/passives/passive_wb_move.png")
+
+modApi:appendAssets("img/effects/", "img/effects/")
