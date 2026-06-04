f88b97ec0f2c360a98a95f82ae6de5d667855d53
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-06-04 10:20:48 -0400

    Adding skill choice UI. Got it working pretty well but still some kinks to iron out

---
+++ RedactedRiceMods/mods/SkillChoices/scripts/init.lua
@@ -0,0 +1,52 @@
+local mod = {
+	id = "redactedrice_SkillChoices",
+	name = "Skill Choices",
+	icon = "mod_icon.png",
+	version = "1.0.0",
+	modApiVersion = "2.9.5",
+	gameVersion = "1.2.93",
+	description = "Choose pilot level up skills from a configurable sized list of options when a pilot levels up.",
+	dependencies = {
+		redactedrice_cplus_plus = "1.2.0",
+		redactedrice_memhack = "1.1.1",
+	},
+}
+
+function mod:metadata()
+	modApi:addGenerationOption(
+		"skill_choice_count",
+		"Level Up Skill Choices",
+		"Number of level up skills offered when a pilot levels up",
+		{
+			values = { 2, 3, 4, 5, 6, 8, 10, 14, 20 },
+			value = 3,
+		}
+	)
+end
+
+function mod:init(options)
+	local skill_choice_ui = require(self.scriptPath .. "skill_choice_ui")
+	self.skill_choice_ui = skill_choice_ui
+end
+
+function mod:load(options, version)
+	-- I want this to be changable mid run so I do this instead of loading
+	-- the typical way which is saved with GAME
+	local globalOptions = nil
+	sdlext.config("modcontent.lua", function(obj)
+		if obj.modOptions and obj.modOptions.redactedrice_SkillChoices then
+			globalOptions = obj.modOptions.redactedrice_SkillChoices.options
+		end
+	end)
+	
+	self.skill_choice_ui.config_options = {}
+	if globalOptions and globalOptions.skill_choice_count then
+		self.skill_choice_ui.config_options.skill_choice_count = globalOptions.skill_choice_count.value
+	else
+		self.skill_choice_ui.config_options.skill_choice_count = 3
+	end
+
+	self.skill_choice_ui:load(options, version)
+end
+
+return mod
