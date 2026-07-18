103575a3c69d734ea564152103b3c872ae42c49b
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-07-17 23:28:37 -0400

    First cut of legendary+. Definitely has some issues with skill choices and enforcements failing

---
+++ RedactedRiceMods/mods/Legendary+/scripts/init.lua
@@ -0,0 +1,23 @@
+local mod = {
+	id = "redactedrice_Legendary+",
+	name = "Legendary Lvl Up Skills",
+	icon = "mod_icon.png",
+	version = "1.0.0",
+	modApiVersion = "2.9.5",
+	gameVersion = "1.2.93",
+	dependencies = {
+		redactedrice_memhack = "1.3.0",
+		redactedrice_cplus_plus = "1.3.1",
+	}
+}
+
+function mod:init()
+	local legendary_plus = require(self.scriptPath .. "legendary_plus")
+	legendary_plus:init()
+end
+
+function mod:load(options, version)
+	legendary_plus:load()
+end
+
+return mod
