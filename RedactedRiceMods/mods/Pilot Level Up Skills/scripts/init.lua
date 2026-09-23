72324d9ee1b437b7a2059328db2807512a56eda8
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-09-22 23:08:50 -0400

    Restructuring heirarchy for separation of mods

---
+++ RedactedRiceMods/mods/Pilot Level Up Skills/scripts/init.lua
@@ -0,0 +1,13 @@
+
+return {
+	id = "redactedrice_pilot_level_up_skills",
+	name = "Pilot Level Up Skills",
+	description = "Custom pilot level up skills by Redacted Rice.",
+	icon = "scripts/icon.png",
+	submodFolders = {"mods/"},
+	version = "1.0.0",
+	modApiVersion = "2.9.5",
+	gameVersion = "1.2.93",
+	init = function() end,
+	load = function() end,
+}
---
+++ RedactedRiceMods/mods/Squads/scripts/init.lua
@@ -0,0 +1,13 @@
+
+return {
+	id = "redactedrice_squads",
+	name = "Squads",
+	description = "Custom squads by Redacted Rice.",
+	icon = "scripts/icon.png",
+	submodFolders = {"mods/"},
+	version = "1.0.0",
+	modApiVersion = "2.9.5",
+	gameVersion = "1.2.93",
+	init = function() end,
+	load = function() end,
+}
