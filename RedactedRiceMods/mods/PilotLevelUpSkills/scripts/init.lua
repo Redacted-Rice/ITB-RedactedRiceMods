560e65a99a48e15f0507996329aa019715313de4
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-09-25 08:58:15 -0400

    Some additional tweaks and consistency changes

--- RedactedRiceMods/mods/PilotLevelUpSkills/scripts/init.lua
+++ RedactedRiceMods/mods/PilotLevelUpSkills/scripts/init.lua
@@ -1,5 +1,5 @@
 
-return {
+local mod = {
 	id = "redactedrice_pilot_level_up_skills",
 	name = "Pilot Level Up Skills",
 	description = "Custom pilot level up skills by Redacted Rice.",
@@ -8,6 +8,12 @@ return {
 	version = "1.0.0",
 	modApiVersion = "2.9.5",
 	gameVersion = "1.2.93",
-	init = function() end,
-	load = function() end,
 }
+
+function mod:init(options)
+end
+
+function mod:load(options, version)
+end
+
+return mod
